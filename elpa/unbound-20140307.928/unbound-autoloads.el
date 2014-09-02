a0bf287bb522ca668b0606150668827fe423561a
Richard Mortier <mort@cantab.net>
2014-09-02 15:15:24 +0100

    emacs: update `unbound` 0.1 -> 20140307.928

---
+++ elpa/unbound-20140307.928/unbound-autoloads.el
@@ -0,0 +1,30 @@
+;;; unbound-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (describe-unbound-keys) "unbound" "unbound.el"
+;;;;;;  (21508 15996 0 0))
+;;; Generated autoloads from unbound.el
+
+(autoload 'describe-unbound-keys "unbound" "\
+Display a list of unbound keystrokes of complexity no greater than MAX.
+Keys are sorted by their complexity; `key-complexity' determines it.
+
+\(fn MAX)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("unbound-pkg.el") (21508 15996 667374
+;;;;;;  0))
+
+;;;***
+
+(provide 'unbound-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; unbound-autoloads.el ends here
