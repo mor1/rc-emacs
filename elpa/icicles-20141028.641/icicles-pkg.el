9232146872be5cce5135762d142a777d31bb14d3
Richard Mortier <mort@cantab.net>
2014-11-01 21:58:07 +0000

    emacs: add icicles mode

---
+++ elpa/icicles-20141028.641/icicles-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "icicles" "20141028.641" "Minibuffer input completion and cycling." 'nil :url "http://www.emacswiki.org/icicles.el" :keywords '("extensions" "help" "abbrev" "local" "minibuffer" "keys" "apropos" "completion" "matching" "regexp" "command"))
