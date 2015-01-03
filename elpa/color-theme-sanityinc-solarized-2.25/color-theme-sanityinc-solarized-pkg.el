6642dd056a51c10aba30ef733df146129eed15a6
Richard Mortier <mort@cantab.net>
2015-01-03 10:54:29 +0000

    emacs: update color-theme-sanityinc-solarized-2.25 to color-theme-sanityinc-solarized-20141205.143

--- elpa/color-theme-sanityinc-solarized-2.25/color-theme-sanityinc-solarized-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "color-theme-sanityinc-solarized" "2.25" "A version of Ethan Schoonover's Solarized themes" 'nil)
--- elpa/color-theme-sanityinc-solarized-20141205.143/color-theme-sanityinc-solarized-autoloads.el
+++ elpa/color-theme-sanityinc-solarized-20141205.143/color-theme-sanityinc-solarized-autoloads.el
@@ -4,18 +4,18 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "color-theme-sanityinc-solarized" "color-theme-sanityinc-solarized.el"
-;;;;;;  (21589 29789 0 0))
+;;;;;;  (21671 46527 0 0))
 ;;; Generated autoloads from color-theme-sanityinc-solarized.el
 
 (when (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
 
 (autoload 'color-theme-sanityinc-solarized-dark "color-theme-sanityinc-solarized" "\
-
+Apply the dark solarized theme.
 
 \(fn)" t nil)
 
 (autoload 'color-theme-sanityinc-solarized-light "color-theme-sanityinc-solarized" "\
-
+Apply the light solarized theme.
 
 \(fn)" t nil)
 
@@ -23,7 +23,7 @@
 
 ;;;### (autoloads nil nil ("color-theme-sanityinc-solarized-pkg.el"
 ;;;;;;  "sanityinc-solarized-dark-theme.el" "sanityinc-solarized-light-theme.el")
-;;;;;;  (21589 29789 631691 0))
+;;;;;;  (21671 46527 608139 0))
 
 ;;;***
 
