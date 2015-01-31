7fe3e3a61ce3bb0439429a29b180fbdca742b797
Richard Mortier <mort@cantab.net>
2015-01-31 22:13:44 +0000

    emacs: update `gh`

--- elpa/gh-20141230.158/gh-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gh" "20141230.158" "A GitHub library for Emacs" '((eieio "1.4") (pcache "0.2.4") (logito "0.1")))
--- elpa/gh-20150126.1125/gh-autoloads.el
+++ elpa/gh-20150126.1125/gh-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "gh-api" "gh-api.el" (21671 46513 0 0))
+;;;### (autoloads nil "gh-api" "gh-api.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-api.el
 
 (require 'eieio)
@@ -14,7 +14,7 @@
 
 ;;;***
 
-;;;### (autoloads nil "gh-auth" "gh-auth.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-auth" "gh-auth.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-auth.el
 
 (require 'eieio)
@@ -27,21 +27,21 @@
 
 ;;;***
 
-;;;### (autoloads nil "gh-cache" "gh-cache.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-cache" "gh-cache.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-cache.el
 
 (require 'eieio)
 
 ;;;***
 
-;;;### (autoloads nil "gh-common" "gh-common.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-common" "gh-common.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-common.el
 
 (require 'eieio)
 
 ;;;***
 
-;;;### (autoloads nil "gh-gist" "gh-gist.el" (21671 46513 0 0))
+;;;### (autoloads nil "gh-gist" "gh-gist.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-gist.el
 
 (require 'eieio)
@@ -55,21 +55,21 @@
 ;;;***
 
 ;;;### (autoloads nil "gh-issue-comments" "gh-issue-comments.el"
-;;;;;;  (21671 46514 0 0))
+;;;;;;  (21709 20861 0 0))
 ;;; Generated autoloads from gh-issue-comments.el
 
 (require 'eieio)
 
 ;;;***
 
-;;;### (autoloads nil "gh-issues" "gh-issues.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-issues" "gh-issues.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-issues.el
 
 (require 'eieio)
 
 ;;;***
 
-;;;### (autoloads nil "gh-oauth" "gh-oauth.el" (21671 46513 0 0))
+;;;### (autoloads nil "gh-oauth" "gh-oauth.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-oauth.el
 
 (require 'eieio)
@@ -78,7 +78,7 @@
 
 ;;;***
 
-;;;### (autoloads nil "gh-orgs" "gh-orgs.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-orgs" "gh-orgs.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-orgs.el
 
 (require 'eieio)
@@ -89,15 +89,15 @@
 
 ;;;***
 
-;;;### (autoloads nil "gh-pull-comments" "gh-pull-comments.el" (21671
-;;;;;;  46514 0 0))
+;;;### (autoloads nil "gh-pull-comments" "gh-pull-comments.el" (21709
+;;;;;;  20861 0 0))
 ;;; Generated autoloads from gh-pull-comments.el
 
 (require 'eieio)
 
 ;;;***
 
-;;;### (autoloads nil "gh-pulls" "gh-pulls.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-pulls" "gh-pulls.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-pulls.el
 
 (require 'eieio)
@@ -108,7 +108,7 @@
 
 ;;;***
 
-;;;### (autoloads nil "gh-repos" "gh-repos.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-repos" "gh-repos.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-repos.el
 
 (require 'eieio)
@@ -121,14 +121,14 @@
 
 ;;;***
 
-;;;### (autoloads nil "gh-url" "gh-url.el" (21671 46513 0 0))
+;;;### (autoloads nil "gh-url" "gh-url.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-url.el
 
 (require 'eieio)
 
 ;;;***
 
-;;;### (autoloads nil "gh-users" "gh-users.el" (21671 46514 0 0))
+;;;### (autoloads nil "gh-users" "gh-users.el" (21709 20861 0 0))
 ;;; Generated autoloads from gh-users.el
 
 (require 'eieio)
@@ -139,8 +139,8 @@
 
 ;;;***
 
-;;;### (autoloads nil nil ("gh-pkg.el" "gh-profile.el" "gh.el") (21671
-;;;;;;  46514 354854 0))
+;;;### (autoloads nil nil ("gh-pkg.el" "gh-profile.el" "gh.el") (21709
+;;;;;;  20861 28695 0))
 
 ;;;***
 
