36ec8a6a1187a94997c3bc7353fade8c37de3ba6
Richard Mortier <mort@cantab.net>
2015-01-31 22:14:31 +0000

    emacs: update `markdown-mode`

--- elpa/markdown-mode-20150121.1229/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20150121.1229/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (21549 43417
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (21709 20847
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
