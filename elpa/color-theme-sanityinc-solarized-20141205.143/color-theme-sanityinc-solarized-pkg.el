d6212f76c9fe4cd38748f8145928662d3e7a8042
Richard Mortier <mort@cantab.net>
2015-02-02 10:11:20 +0000

    emacs: update `color-theme-sanityinc-solarized`

--- elpa/color-theme-sanityinc-solarized-20141205.143/color-theme-sanityinc-solarized-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "color-theme-sanityinc-solarized" "20141205.143" "A version of Ethan Schoonover's Solarized themes" 'nil)
---
+++ elpa/color-theme-sanityinc-solarized-20150201.1326/color-theme-sanityinc-solarized-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "color-theme-sanityinc-solarized" "20150201.1326" "A version of Ethan Schoonover's Solarized themes" 'nil)
