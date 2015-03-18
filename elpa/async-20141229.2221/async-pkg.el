d0f47623da9b6280d0b7c1984b768c97b489c724
Richard Mortier <mort@cantab.net>
2015-03-18 11:01:28 +0000

    emacs: update `async`

--- elpa/async-20141229.2221/async-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "async" "20141229.2221" "Asynchronous processing in Emacs" 'nil)
---
+++ elpa/async-20150203.2127/async-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "async" "20150203.2127" "Asynchronous processing in Emacs" 'nil)
