8e426779f4ad3583e6a32d93b2d43d01bd4c5f74
Richard Mortier <mort@cantab.net>
2015-03-18 11:01:27 +0000

    emacs: update `diff-hl`

--- elpa/diff-hl-20150113.2113/diff-hl-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "diff-hl" "20150113.2113" "Highlight uncommitted changes" '((cl-lib "0.2")) :url "https://github.com/dgutov/diff-hl" :keywords '("vc" "diff"))
