c1c6782ea54aa8d864bd14f868cc2797aefc5c97
Richard Mortier <mort@cantab.net>
2015-04-02 17:34:33 +0100

    emacs: update `epl`

--- elpa/epl-20150326.1212/epl-autoloads.el
+++ elpa/epl-20150326.1212/epl-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("epl.el") (21769 22739 415528 0))
+;;;### (autoloads nil nil ("epl.el") (21789 23063 201572 0))
 
 ;;;***
 
