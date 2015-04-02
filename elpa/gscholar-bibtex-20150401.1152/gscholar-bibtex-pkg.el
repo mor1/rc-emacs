14b39cbef76362863110e1a4885b961ff9140846
Richard Mortier <mort@cantab.net>
2015-04-02 17:38:50 +0100

    emacs: add `gscholar-bibtex`

---
+++ elpa/gscholar-bibtex-20150401.1152/gscholar-bibtex-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gscholar-bibtex" "20150401.1152" "Retrieve BibTeX from Google Scholar and other online sources(ACM, IEEE, DBLP)" 'nil :keywords '("extensions"))
