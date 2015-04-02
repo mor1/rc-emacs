3254b5a15ac1d4708659a42b89becb0c8a1bf54a
Richard Mortier <mort@cantab.net>
2015-04-02 17:34:37 +0100

    emacs: update `coffee-mode`

--- elpa/coffee-mode-20150318.1623/coffee-mode-autoloads.el
+++ elpa/coffee-mode-20150318.1623/coffee-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21763 5598
+;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (21789 23069
 ;;;;;;  0 0))
 ;;; Generated autoloads from coffee-mode.el
 
