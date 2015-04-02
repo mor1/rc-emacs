a04c599b9357530bece47b6804aa707b368d19e9
Richard Mortier <mort@cantab.net>
2015-04-02 17:34:35 +0100

    emacs: update `git-commit-mode`

--- elpa/git-commit-mode-20150330.1048/git-commit-mode-autoloads.el
+++ elpa/git-commit-mode-20150330.1048/git-commit-mode-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-commit-mode" "git-commit-mode.el" (21589
-;;;;;;  21567 0 0))
+;;;### (autoloads nil "git-commit-mode" "git-commit-mode.el" (21789
+;;;;;;  23059 0 0))
 ;;; Generated autoloads from git-commit-mode.el
 
 (autoload 'git-commit-mode "git-commit-mode" "\
