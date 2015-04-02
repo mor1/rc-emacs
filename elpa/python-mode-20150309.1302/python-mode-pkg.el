9812ccf8fb9689d74402e728f261d003898927ec
Richard Mortier <mort@cantab.net>
2015-04-02 17:33:33 +0100

    emacs: update `python-mode`

--- elpa/python-mode-20150309.1302/python-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "python-mode" "20150309.1302" "Python major mode" 'nil)
---
+++ elpa/python-mode-20150327.438/python-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "python-mode" "20150327.438" "Python major mode" 'nil)
