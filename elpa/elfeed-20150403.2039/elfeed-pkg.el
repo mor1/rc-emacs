40d4f390e5da54f722d93def5268ebe403a57834
Richard Mortier <mort@cantab.net>
2015-04-07 17:55:43 +0100

    emacs: update `elfeed`

--- elpa/elfeed-20150403.2039/elfeed-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "elfeed" "20150403.2039" "an Emacs Atom/RSS feed reader" '((emacs "24.1")))
---
+++ elpa/elfeed-20150404.545/elfeed-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elfeed" "20150404.545" "an Emacs Atom/RSS feed reader" '((emacs "24.1")))
