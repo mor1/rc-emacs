29e44e20c6da109ff2228174a5f0014ca73878b5
Richard Mortier <mort@cantab.net>
2015-04-09 14:53:45 +0100

    emacs: add `latex-pretty-symbols`

---
+++ elpa/latex-pretty-symbols-20150409.240/latex-pretty-symbols-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "latex-pretty-symbols" "20150409.240" "Display many latex symbols as their unicode counterparts" 'nil :url "https://bitbucket.org/mortiferus/latex-pretty-symbols.el" :keywords '("convenience" "display"))
