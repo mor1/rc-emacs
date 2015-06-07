ee1ffcbb6a66013c88eb61366610397fdec6d6b8
Richard Mortier <mort@cantab.net>
2015-06-07 21:52:38 +0100

    emacs: update `d-mode`

---
+++ elpa/d-mode-20150605.740/d-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "d-mode" "20150605.740" "D Programming Language major mode for (X)Emacs" 'nil :keywords '("d" "programming" "language" "emacs" "cc-mode"))
