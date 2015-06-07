8873b7527317ebc01cecd8c21c8c4e527be61d36
Richard Mortier <mort@cantab.net>
2015-06-07 21:51:02 +0100

    emacs: update `pkg-info`

--- elpa/pkg-info-20150517.443/pkg-info-autoloads.el
+++ elpa/pkg-info-20150517.443/pkg-info-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "pkg-info" "pkg-info.el" (21769 22745 0 0))
+;;;### (autoloads nil "pkg-info" "pkg-info.el" (21849 64436 0 0))
 ;;; Generated autoloads from pkg-info.el
 
 (autoload 'pkg-info-library-original-version "pkg-info" "\
