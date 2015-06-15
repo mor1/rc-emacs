bcd13982a1299d3eb8bc5b9c339f8065fafb1296
Richard Mortier <mort@cantab.net>
2015-06-15 20:22:09 +0100

    emacs: add `bbdb-android`

---
+++ elpa/bbdb-android-20150613.1516/bbdb-android-autoloads.el
@@ -0,0 +1,16 @@
+;;; bbdb-android-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("bbdb-android.el") (21886 54433 982546
+;;;;;;  0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; bbdb-android-autoloads.el ends here
---
+++ elpa/bbdb-android-20150613.1516/bbdb-android-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bbdb-android" "20150613.1516" "Android phone contacts import/export for BBDB" '((bbdb-vcard "0.4.1")) :url "https://github.com/tumashu/bbdb-android")
