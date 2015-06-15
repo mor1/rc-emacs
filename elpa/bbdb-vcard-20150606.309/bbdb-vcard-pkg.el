c03e0e68677c09eeb3a3389969f56d29a4811a24
Richard Mortier <mort@cantab.net>
2015-06-15 20:21:48 +0100

    emacs: update `bbdb-vcard`

--- elpa/bbdb-vcard-20150606.309/bbdb-vcard-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bbdb-vcard" "20150606.309" "vCard import/export for BBDB" '((bbdb "3.0")) :url "http://github.com/vgeddes/bbdb-vcard" :keywords '("data" "calendar" "mail" "news"))
---
+++ elpa/bbdb-vcard-20150611.2336/bbdb-vcard-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bbdb-vcard" "20150611.2336" "vCard import/export for BBDB" '((bbdb "3.0")) :url "http://github.com/vgeddes/bbdb-vcard" :keywords '("data" "calendar" "mail" "news"))
