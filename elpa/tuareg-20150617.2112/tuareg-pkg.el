d817fb21eaac329ebe014fca81c2b0ba5fb18463
Richard Mortier <mort@cantab.net>
2015-06-21 22:10:46 +0100

    emacs: update `tuareg`

---
+++ elpa/tuareg-20150617.2112/tuareg-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "tuareg" "20150617.2112" "OCaml mode for Emacs." '((caml "3.12.0.1")) :url "https://github.com/ocaml/tuareg" :keywords '("ocaml" "languages"))
