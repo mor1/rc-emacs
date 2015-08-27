de026007a5c72371dfdc31f90ecb00407e53a240
Richard Mortier <mort@cantab.net>
2015-08-27 10:04:14 +0100

    emacs: update `s`

--- elpa/s-20150825.50/s-autoloads.el
+++ elpa/s-20150825.50/s-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("s.el") (21549 43380 305199 0))
+;;;### (autoloads nil nil ("s.el") (21982 52540 872049 0))
 
 ;;;***
 
