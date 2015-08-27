7a3cf3dd058674c3adbfc00aa59d0668b7c99ba1
Richard Mortier <mort@cantab.net>
2015-08-27 10:04:02 +0100

    emacs: update `irony`

--- elpa/irony-20150824.1243/irony-pkg.el
+++ elpa/irony-20150824.1243/irony-pkg.el
@@ -1,4 +1,4 @@
-(define-package "irony" "20150810.128" "C/C++ minor mode powered by libclang"
+(define-package "irony" "20150824.1243" "C/C++ minor mode powered by libclang"
   '((cl-lib "0.5")
     (json "1.2"))
   :url "https://github.com/Sarcasm/irony-mode" :keywords
