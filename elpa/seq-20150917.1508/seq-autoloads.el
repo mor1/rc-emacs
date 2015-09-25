463e394749705786c502785ebad66ec649a239ee
Richard Mortier <mort@cantab.net>
2015-09-25 15:06:23 +0100

    emacs: update `seq`

--- elpa/seq-20150917.1508/seq-autoloads.el
+++ elpa/seq-20150917.1508/seq-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("seq.el") (21960 33362 411203 0))
+;;;### (autoloads nil nil ("seq.el") (22021 3220 424607 0))
 
 ;;;***
 
