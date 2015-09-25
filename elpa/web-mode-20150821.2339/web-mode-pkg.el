dad6efb42244501dbf1d9cfe0ba7fca80d19a266
Richard Mortier <mort@cantab.net>
2015-09-25 15:06:24 +0100

    emacs: update `web-mode`

--- elpa/web-mode-20150821.2339/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20150821.2339" "major mode for editing web templates" 'nil :stars '(#("781" 0 3 (font-lock-face paradox-star-face))) :url "http://web-mode.org" :keywords '("languages"))
