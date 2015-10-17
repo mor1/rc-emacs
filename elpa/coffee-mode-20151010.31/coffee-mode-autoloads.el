b8f7ebaeaf5ec49237bcf4fdc0c2b0c51f81947a
Richard Mortier <mort@cantab.net>
2015-10-17 16:49:26 +0100

    emacs: update `coffee-mode`

--- elpa/coffee-mode-20151010.31/coffee-mode-autoloads.el
+++ elpa/coffee-mode-20151010.31/coffee-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (22021 3255
+;;;### (autoloads nil "coffee-mode" "coffee-mode.el" (22050 23051
 ;;;;;;  0 0))
 ;;; Generated autoloads from coffee-mode.el
 
--- elpa/coffee-mode-20151010.31/coffee-mode.el
+++ elpa/coffee-mode-20151010.31/coffee-mode.el
@@ -2,8 +2,8 @@
 
 ;; Copyright (C) 2010 Chris Wanstrath
 
-;; Version: 0.6.0
-;; Package-Version: 20150906.2225
+;; Version: 0.6.1
+;; Package-Version: 20151010.31
 ;; Keywords: CoffeeScript major mode
 ;; Author: Chris Wanstrath <chris@ozmm.org>
 ;; URL: http://github.com/defunkt/coffee-mode
@@ -43,7 +43,7 @@
 ;; Customizable Variables
 ;;
 
-(defconst coffee-mode-version "0.6.0"
+(defconst coffee-mode-version "0.6.1"
   "The version of `coffee-mode'.")
 
 (defgroup coffee nil
@@ -125,6 +125,11 @@ with CoffeeScript."
 (defcustom coffee-after-compile-hook nil
   "Hook called after compile to Javascript"
   :type 'hook
+  :group 'coffee)
+
+(defcustom coffee-indent-like-python-mode nil
+  "Indent like python-mode."
+  :type 'boolean
   :group 'coffee)
 
 (defvar coffee-mode-map
@@ -248,11 +253,11 @@ called `coffee-compiled-buffer-name'."
     (goto-char (point-min))
     (let ((line (buffer-substring-no-properties (point) (line-end-position))))
       (when (string-match "[0-9.]+\\'" line)
-        (string-to-number (match-string-no-properties 0 line))))))
+        (match-string-no-properties 0 line)))))
 
 (defun coffee--map-file-name (coffee-file)
   (let* ((version (coffee--coffeescript-version))
-         (extension (if (>= version 1.8) ".js.map" ".map")))
+         (extension (if (version<= "1.8" version) ".js.map" ".map")))
     ;; foo.js: foo.js.map(>= 1.8), foo.map(< 1.8)
     (concat (file-name-sans-extension coffee-file) extension)))
 
@@ -671,6 +676,15 @@ output in a compilation buffer."
   (when (< (current-column) (current-indentation))
     (back-to-indentation)))
 
+(defun coffee--indent-line-like-python-mode (prev-indent repeated)
+  (let ((next-indent (- (current-indentation) coffee-tab-width))
+        (indent-p (coffee-line-wants-indent)))
+    (if repeated
+        (if (< next-indent 0)
+            (+ prev-indent (if indent-p coffee-tab-width 0))
+          next-indent)
+      (+ prev-indent (if indent-p coffee-tab-width 0)))))
+
 (defun coffee-indent-line ()
   "Indent current line as CoffeeScript."
   (interactive)
@@ -681,11 +695,15 @@ output in a compilation buffer."
          begin-indents)
     (if (and type (setq begin-indents (coffee--find-indents type limit '<)))
         (setq indent-size (coffee--decide-indent curindent begin-indents '>))
-      (let ((prev-indent (coffee-previous-indent))
-            (next-indent-size (+ curindent coffee-tab-width)))
-        (if (> (- next-indent-size prev-indent) coffee-tab-width)
-            (setq indent-size 0)
-          (setq indent-size (+ curindent coffee-tab-width)))))
+      (if coffee-indent-like-python-mode
+          (setq indent-size
+                (coffee--indent-line-like-python-mode
+                 (coffee-previous-indent) (eq last-command this-command)))
+        (let ((prev-indent (coffee-previous-indent))
+              (next-indent-size (+ curindent coffee-tab-width)))
+          (if (> (- next-indent-size prev-indent) coffee-tab-width)
+              (setq indent-size 0)
+            (setq indent-size (+ curindent coffee-tab-width))))))
     (coffee--indent-insert-spaces indent-size)))
 
 (defun coffee-previous-indent ()
