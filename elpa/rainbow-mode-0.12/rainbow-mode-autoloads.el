af82e8375669c8b48891b1475d3bdef7df4711be
Richard Mortier <mort@cantab.net>
2015-10-17 16:49:39 +0100

    emacs: update `rainbow-mode`

--- elpa/rainbow-mode-0.12/rainbow-mode-autoloads.el
+++ elpa/rainbow-mode-0.12/rainbow-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "rainbow-mode" "rainbow-mode.el" (21763 4812
+;;;### (autoloads nil "rainbow-mode" "rainbow-mode.el" (22050 23011
 ;;;;;;  0 0))
 ;;; Generated autoloads from rainbow-mode.el
 
