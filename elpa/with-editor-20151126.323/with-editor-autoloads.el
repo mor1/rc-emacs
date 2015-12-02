14cc164022d7f5bb259a94b1296375ccf655bf9c
Richard Mortier <mort@cantab.net>
2015-12-02 18:27:18 +0000

    emacs: update `with-editor`

--- elpa/with-editor-20151126.323/with-editor-autoloads.el
+++ elpa/with-editor-20151126.323/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22059 16099 253888 0))
+;;;;;;  (22111 13627 465849 0))
 
 ;;;***
 
