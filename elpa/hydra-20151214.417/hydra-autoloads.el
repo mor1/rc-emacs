e0f543f609872a460741c3e7d8e60983c1c6c292
Richard Mortier <mort@cantab.net>
2016-01-05 09:24:39 +0000

    emacs: update `hydra`

--- elpa/hydra-20151214.417/hydra-autoloads.el
+++ elpa/hydra-20151214.417/hydra-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "hydra" "hydra.el" (22111 13672 0 0))
+;;;### (autoloads nil "hydra" "hydra.el" (22126 64771 0 0))
 ;;; Generated autoloads from hydra.el
 
 (autoload 'defhydra "hydra" "\
@@ -63,7 +63,7 @@ result of `defhydra'.
 ;;;***
 
 ;;;### (autoloads nil nil ("hydra-examples.el" "hydra-ox.el" "hydra-pkg.el"
-;;;;;;  "lv.el") (22111 13672 526743 0))
+;;;;;;  "lv.el") (22126 64771 775920 0))
 
 ;;;***
 
