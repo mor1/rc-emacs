474d9681e8cdc46fce812147e305b3399ecfbe12
Richard Mortier <mort@cantab.net>
2016-01-12 16:03:42 +0000

    emacs: add `auctex`

---
+++ elpa/auctex-latexmk-20151217.557/auctex-latexmk-pkg.el
@@ -0,0 +1,1 @@
+(define-package "auctex-latexmk" "20151217.557" "Add LatexMk support to AUCTeX" '((auctex "11.87")) :stars '(#("26" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/tom-tan/auctex-latexmk/" :keywords '("tex"))
