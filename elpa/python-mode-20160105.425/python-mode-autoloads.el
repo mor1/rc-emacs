519355ec3072989f1ff82cf540bfc80156e7a720
Richard Mortier <mort@cantab.net>
2016-02-14 00:37:22 +0000

    emacs: update `python-mode`

--- elpa/python-mode-20160105.425/python-mode-autoloads.el
+++ elpa/python-mode-20160105.425/python-mode-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil nil ("python-mode-pkg.el" "python-mode.el")
-;;;;;;  (22155 41875 581090 0))
+;;;;;;  (22167 36258 564999 0))
 
 ;;;***
 
