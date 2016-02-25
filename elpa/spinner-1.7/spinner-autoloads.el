da1d06cfb0af1cabc1ed65ad86bea9ae0db675d9
Richard Mortier <mort@cantab.net>
2016-02-25 14:45:23 +0000

    emacs: update `spinner`

--- elpa/spinner-1.7/spinner-autoloads.el
+++ elpa/spinner-1.7/spinner-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "spinner" "spinner.el" (21964 52230 0 0))
+;;;### (autoloads nil "spinner" "spinner.el" (22211 26045 0 0))
 ;;; Generated autoloads from spinner.el
 
 (autoload 'spinner-create "spinner" "\
