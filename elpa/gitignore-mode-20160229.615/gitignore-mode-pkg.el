14d3a6e41f8c1f5b6251e23e408c0d5e12696e72
Richard Mortier <mort@cantab.net>
2016-03-15 09:09:40 +0000

    emacs: update `gitignore-mode`

---
+++ elpa/gitignore-mode-20160229.615/gitignore-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "gitignore-mode" "20160229.615" "Major mode for editing .gitignore files" 'nil :stars '(#("160" 0 3 (font-lock-face paradox-star-face))) :url "https://github.com/magit/git-modes" :keywords '("convenience" "vc" "git"))
