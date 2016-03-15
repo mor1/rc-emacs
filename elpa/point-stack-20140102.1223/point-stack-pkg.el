ba80170bffd093f229c5c6576599af1d1c0c8fc0
Richard Mortier <mort@cantab.net>
2016-03-15 09:09:40 +0000

    emacs: update `point-stack`

--- elpa/point-stack-20140102.1223/point-stack-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "point-stack" "20140102.1223" "A forward/back stack for point" 'nil :stars '(#("10" 0 2 (font-lock-face paradox-star-face))))
--- elpa/point-stack-20141225.2110/point-stack-autoloads.el
+++ elpa/point-stack-20141225.2110/point-stack-autoloads.el
@@ -3,24 +3,31 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "point-stack" "point-stack.el" (21986 8493
+;;;### (autoloads nil "point-stack" "point-stack.el" (22247 53516
 ;;;;;;  0 0))
 ;;; Generated autoloads from point-stack.el
 
 (autoload 'point-stack-push "point-stack" "\
-Push current buffer, point, and scroll position onto stack.
+Push current buffer, point, and window scroll position onto the stack.
 
 \(fn)" t nil)
 
 (autoload 'point-stack-pop "point-stack" "\
-Push current location onto forward stack, move to previous location.
+Push current location on forward stack, move to previous location.
 
 \(fn)" t nil)
 
 (autoload 'point-stack-forward-stack-pop "point-stack" "\
-Push current location onto stack, pop and move to location from forward stack.
+Push current location on stack, pop and move to location from forward stack.
 
 \(fn)" t nil)
+
+(autoload 'point-stack-setup-advices "point-stack" "\
+Advise navigation functions to call `point-stack-push' before
+any navigation is made. This way, it can be used as a replacement
+for the global mark ring.
+
+\(fn)" nil nil)
 
 ;;;***
 
