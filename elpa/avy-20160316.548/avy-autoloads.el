4f3acfde2c7c9a045a68e9e004bf085be3230e82
Richard Mortier <mort@cantab.net>
2016-03-17 12:03:22 +0000

    emacs: update `avy`

--- elpa/avy-20160316.548/avy-autoloads.el
+++ elpa/avy-20160316.548/avy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "avy" "avy.el" (22241 19620 0 0))
+;;;### (autoloads nil "avy" "avy.el" (22250 40165 0 0))
 ;;; Generated autoloads from avy.el
 
 (autoload 'avy-goto-char "avy" "\
