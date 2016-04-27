4820eea1d2da5879d23352105391a820235d6934
Richard Mortier <mort@cantab.net>
2016-04-27 22:01:55 +0100

    emacs: update `paradox`

--- elpa/paradox-20160323.1210/paradox-autoloads.el
+++ elpa/paradox-20160323.1210/paradox-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "paradox" "paradox.el" (22241 19597 0 0))
+;;;### (autoloads nil "paradox" "paradox.el" (22305 3464 0 0))
 ;;; Generated autoloads from paradox.el
 
 (autoload 'paradox-list-packages "paradox" "\
@@ -62,7 +62,7 @@ empty.  Passing a non-nil REFRESH argument forces this update.
 
 ;;;### (autoloads nil nil ("paradox-commit-list.el" "paradox-core.el"
 ;;;;;;  "paradox-execute.el" "paradox-github.el" "paradox-menu.el"
-;;;;;;  "paradox-pkg.el") (22241 19597 436211 0))
+;;;;;;  "paradox-pkg.el") (22305 3464 306081 0))
 
 ;;;***
 
