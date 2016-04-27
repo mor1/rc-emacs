f70321a61f0bddd927d3130e5a38aebc640eb16e
Richard Mortier <mort@cantab.net>
2016-04-27 22:01:54 +0100

    emacs: update `git-commit`

--- elpa/git-commit-20160425.430/git-commit-autoloads.el
+++ elpa/git-commit-20160425.430/git-commit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (22211 26105 0
+;;;### (autoloads nil "git-commit" "git-commit.el" (22305 3478 0
 ;;;;;;  0))
 ;;; Generated autoloads from git-commit.el
 
@@ -27,7 +27,7 @@ provide such a commit message.
 
 ;;;***
 
-;;;### (autoloads nil nil ("git-commit-pkg.el") (22211 26105 49123
+;;;### (autoloads nil nil ("git-commit-pkg.el") (22305 3478 597364
 ;;;;;;  0))
 
 ;;;***
