787a9d0522076a32573c250c4f179a21acfc5e10
Richard Mortier <mort@cantab.net>
2016-04-27 22:01:54 +0100

    emacs: update `auto-complete`

--- elpa/auto-complete-20160416.604/auto-complete-autoloads.el
+++ elpa/auto-complete-20160416.604/auto-complete-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "auto-complete" "auto-complete.el" (22247 53532
+;;;### (autoloads nil "auto-complete" "auto-complete.el" (22305 3486
 ;;;;;;  0 0))
 ;;; Generated autoloads from auto-complete.el
 
@@ -41,7 +41,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 ;;;***
 
 ;;;### (autoloads nil "auto-complete-config" "auto-complete-config.el"
-;;;;;;  (22247 53532 0 0))
+;;;;;;  (22305 3486 0 0))
 ;;; Generated autoloads from auto-complete-config.el
 
 (autoload 'ac-config-default "auto-complete-config" "\
@@ -51,7 +51,7 @@ See `auto-complete-mode' for more information on Auto-Complete mode.
 
 ;;;***
 
-;;;### (autoloads nil nil ("auto-complete-pkg.el") (22247 53532 515019
+;;;### (autoloads nil nil ("auto-complete-pkg.el") (22305 3486 518982
 ;;;;;;  0))
 
 ;;;***
