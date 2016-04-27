237d3630404f9a8e0695eae7f5e7dc73deb93215
Richard Mortier <mort@cantab.net>
2016-04-27 22:01:55 +0100

    emacs: update `use-package`

--- elpa/use-package-20160403.1129/use-package-autoloads.el
+++ elpa/use-package-20160403.1129/use-package-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "use-package" "use-package.el" (22226 63945
+;;;### (autoloads nil "use-package" "use-package.el" (22305 3456
 ;;;;;;  0 0))
 ;;; Generated autoloads from use-package.el
 
