5700169737fe7d4fadba6f5735cbf6d50e109f27
Richard Mortier <mort@cantab.net>
2016-05-28 22:23:44 +0100

    emacs: update `diff-hl`

--- elpa/diff-hl-20160517.1631/diff-hl-autoloads.el
+++ elpa/diff-hl-20160517.1631/diff-hl-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil "diff-hl" "diff-hl.el" (22211 26111 0 0))
+;;;### (autoloads nil "diff-hl" "diff-hl.el" (22346 2932 0 0))
 ;;; Generated autoloads from diff-hl.el
 
 (autoload 'diff-hl-mode "diff-hl" "\
@@ -39,7 +39,7 @@ See `diff-hl-mode' for more information on Diff-Hl mode.
 
 ;;;***
 
-;;;### (autoloads nil "diff-hl-amend" "diff-hl-amend.el" (22211 26111
+;;;### (autoloads nil "diff-hl-amend" "diff-hl-amend.el" (22346 2932
 ;;;;;;  0 0))
 ;;; Generated autoloads from diff-hl-amend.el
 
@@ -74,7 +74,7 @@ See `diff-hl-amend-mode' for more information on Diff-Hl-Amend mode.
 
 ;;;***
 
-;;;### (autoloads nil "diff-hl-dired" "diff-hl-dired.el" (22211 26111
+;;;### (autoloads nil "diff-hl-dired" "diff-hl-dired.el" (22346 2932
 ;;;;;;  0 0))
 ;;; Generated autoloads from diff-hl-dired.el
 
@@ -90,8 +90,8 @@ Toggle VC diff highlighting on the side of a Dired window.
 
 ;;;***
 
-;;;### (autoloads nil "diff-hl-flydiff" "diff-hl-flydiff.el" (22211
-;;;;;;  26111 0 0))
+;;;### (autoloads nil "diff-hl-flydiff" "diff-hl-flydiff.el" (22346
+;;;;;;  2932 0 0))
 ;;; Generated autoloads from diff-hl-flydiff.el
 
 (defvar diff-hl-flydiff-mode nil "\
@@ -110,8 +110,8 @@ Highlight diffs on-the-fly
 
 ;;;***
 
-;;;### (autoloads nil "diff-hl-margin" "diff-hl-margin.el" (22211
-;;;;;;  26111 0 0))
+;;;### (autoloads nil "diff-hl-margin" "diff-hl-margin.el" (22346
+;;;;;;  2932 0 0))
 ;;; Generated autoloads from diff-hl-margin.el
 
 (defvar diff-hl-margin-mode nil "\
@@ -130,8 +130,7 @@ Toggle displaying `diff-hl-mode' highlights on the margin.
 
 ;;;***
 
-;;;### (autoloads nil nil ("diff-hl-pkg.el") (22211 26111 250023
-;;;;;;  0))
+;;;### (autoloads nil nil ("diff-hl-pkg.el") (22346 2932 591162 0))
 
 ;;;***
 
--- elpa/diff-hl-20160517.1631/diff-hl-pkg.el
+++ elpa/diff-hl-20160517.1631/diff-hl-pkg.el
@@ -1,4 +1,4 @@
-(define-package "diff-hl" "20160203.1501" "Highlight uncommitted changes using VC"
+(define-package "diff-hl" "20160517.1631" "Highlight uncommitted changes using VC"
   '((cl-lib "0.2"))
   :url "https://github.com/dgutov/diff-hl" :keywords
   '("vc" "diff"))
