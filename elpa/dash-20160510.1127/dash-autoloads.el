df019221bf8acae6d864a4af6a1020e12f4e4dfc
Richard Mortier <mort@cantab.net>
2016-05-28 22:23:44 +0100

    emacs: update `dash`

--- elpa/dash-20160510.1127/dash-autoloads.el
+++ elpa/dash-20160510.1127/dash-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("dash.el") (22241 19616 933498 0))
+;;;### (autoloads nil nil ("dash.el") (22346 2934 612626 0))
 
 ;;;***
 
