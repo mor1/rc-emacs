c2cd70c665d1a3bcfacaf52f5ababc7c7e32e8f2
Richard Mortier <mort@cantab.net>
2016-05-28 22:23:45 +0100

    emacs: update `popup`

--- elpa/popup-20160527.752/popup-autoloads.el
+++ elpa/popup-20160527.752/popup-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
-;;;### (autoloads nil nil ("popup.el") (22305 3461 93795 0))
+;;;### (autoloads nil nil ("popup.el") (22346 2824 595164 0))
 
 ;;;***
 
