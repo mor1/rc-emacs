95ba8beeff2a8b3a02f23ce58b38005f6ecdb449
Richard Mortier <mort@cantab.net>
2016-08-18 11:51:04 +0100

    emacs: update `go-mode`

--- elpa/go-mode-20160715.205/go-mode-autoloads.el
+++ elpa/go-mode-20160715.205/go-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 
 
-;;;### (autoloads nil "go-mode" "go-mode.el" (22346 2922 0 0))
+;;;### (autoloads nil "go-mode" "go-mode.el" (22444 32605 0 0))
 ;;; Generated autoloads from go-mode.el
 
 (autoload 'go-mode "go-mode" "\
@@ -95,7 +95,8 @@ Tries to look for a URL at point.
 
 ;;;***
 
-;;;### (autoloads nil nil ("go-mode-pkg.el") (22346 2922 396706 0))
+;;;### (autoloads nil nil ("go-mode-pkg.el") (22444 32605 479889
+;;;;;;  0))
 
 ;;;***
 
