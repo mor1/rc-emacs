b01a020361d61d42d68164cbcd8e40427d22d9a0
Richard Mortier <mort@cantab.net>
2016-08-18 11:51:03 +0100

    emacs: update `aggressive-indent`

--- elpa/aggressive-indent-20160608.1625/aggressive-indent-autoloads.el
+++ elpa/aggressive-indent-20160608.1625/aggressive-indent-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
 
 ;;;### (autoloads nil "aggressive-indent" "aggressive-indent.el"
-;;;;;;  (22346 2982 0 0))
+;;;;;;  (22444 32626 0 0))
 ;;; Generated autoloads from aggressive-indent.el
 
 (autoload 'aggressive-indent-indent-defun "aggressive-indent" "\
