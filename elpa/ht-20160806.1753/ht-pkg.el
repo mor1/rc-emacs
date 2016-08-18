0d3ea30fe6ca56b0a874c2ae52bcbeb1d70b7e5c
Richard Mortier <mort@cantab.net>
2016-08-18 11:51:06 +0100

    emacs: add `ht`

---
+++ elpa/ht-20160806.1753/ht-pkg.el
@@ -0,0 +1,1 @@
+(define-package "ht" "20160806.1753" "The missing hash table library for Emacs" '((dash "2.12.0")) :stars '(#("63" 0 2 (font-lock-face paradox-star-face))) :keywords '("hash table" "hash map" "hash"))
