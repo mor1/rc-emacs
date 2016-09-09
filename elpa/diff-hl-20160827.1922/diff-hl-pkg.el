d19defe6f3f2677ecdb118c75caeaf1eb6c5de81
Richard Mortier <mort@cantab.net>
2016-09-09 20:03:11 +0100

    emacs: update `diff-hl`

--- elpa/diff-hl-20160827.1922/diff-hl-pkg.el
+++ elpa/diff-hl-20160827.1922/diff-hl-pkg.el
@@ -1,4 +1,4 @@
-(define-package "diff-hl" "20160622.1816" "Highlight uncommitted changes using VC"
+(define-package "diff-hl" "20160827.1922" "Highlight uncommitted changes using VC"
   '((cl-lib "0.2"))
   :url "https://github.com/dgutov/diff-hl" :keywords
   '("vc" "diff"))
