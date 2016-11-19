6eec183970d4d8d965ab0bb97b42f9ac479044af
Richard Mortier <mort@cantab.net>
2016-11-19 14:07:29 +0000

    emacs: update `dash`

--- elpa/dash-20161116.920/dash-autoloads.el
+++ elpa/dash-20161116.920/dash-autoloads.el
@@ -1,10 +1,9 @@
 ;;; dash-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (22483 1175 677272
-;;;;;;  0))
+;;;### (autoloads nil nil ("dash.el") (22576 23564 0 0))
 
 ;;;***
 
