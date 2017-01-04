798f9160a05d42f08735a22131e94d7a38b8a18f
Richard Mortier <mort@cantab.net>
2017-01-04 14:16:47 +0000

    emacs: fancy new OPAM generated configuration

--- init.el
+++ init.el
@@ -1042,6 +1042,10 @@ This command is convenient when reading novel, documentation."
   )
 ;; (ad-activate 'load)
 
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
+
 (load custom-file)
 
 (put 'narrow-to-region 'disabled nil)
