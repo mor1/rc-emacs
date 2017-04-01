04400eb7198498df5bfab5081e0af71e87dc1ea4
Richard Mortier <mort@cantab.net>
2017-04-01 11:30:24 +0100

    emacs: add `let-alist`

--- elpa/let-alist-1.0.5/let-alist-autoloads.el
+++ elpa/let-alist-1.0.5/let-alist-autoloads.el
@@ -1,9 +1,9 @@
 ;;; let-alist-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "let-alist" "let-alist.el" (21886 54396 0 0))
+;;;### (autoloads nil "let-alist" "let-alist.el" (22721 10695 0 0))
 ;;; Generated autoloads from let-alist.el
 
 (autoload 'let-alist "let-alist" "\
@@ -21,10 +21,10 @@ For instance, the following code
 
 essentially expands to
 
-  (let ((.title (cdr (assq 'title alist)))
-        (.body  (cdr (assq 'body alist)))
-        (.site  (cdr (assq 'site alist)))
-        (.site.contents (cdr (assq 'contents (cdr (assq 'site alist))))))
+  (let ((.title (cdr (assq \\='title alist)))
+        (.body  (cdr (assq \\='body alist)))
+        (.site  (cdr (assq \\='site alist)))
+        (.site.contents (cdr (assq \\='contents (cdr (assq \\='site alist))))))
     (if (and .title .body)
         .body
       .site
