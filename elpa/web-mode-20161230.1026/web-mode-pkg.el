71f2dfe39c0ad3d5c172c744fc9bea9bfbf668d8
Richard Mortier <mort@cantab.net>
2017-04-01 11:30:25 +0100

    emacs: add `web-mode`

--- elpa/web-mode-20161230.1026/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20161230.1026" "major mode for editing web templates" 'nil :stars '(#("879" 0 3 (font-lock-face paradox-star-face))) :url "http://web-mode.org" :keywords '("languages"))
--- elpa/web-mode-20170330.2328/web-mode-autoloads.el
+++ elpa/web-mode-20170330.2328/web-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (22636 51166 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (22751 32757 0 0))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
