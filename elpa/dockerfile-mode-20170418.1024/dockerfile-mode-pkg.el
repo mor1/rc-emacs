1c57cc3d3c07671d5355187d0454a63214a23f2f
Richard Mortier <mort@cantab.net>
2017-04-19 00:20:46 +0100

    emacs: add `dockerfile-mode`

---
+++ elpa/dockerfile-mode-20170418.1024/dockerfile-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dockerfile-mode" "20170418.1024" "Major mode for editing Docker's Dockerfiles" 'nil :stars '(#("137" 0 3 (font-lock-face paradox-star-face))))
