27d86685d639abb83dfec2187fdb498fb8e0fb3d
Richard Mortier <mort@cantab.net>
2017-05-02 07:12:43 +0100

    emacs: add `magit`

--- elpa/magit-popup-20170420.544/magit-popup-autoloads.el
+++ elpa/magit-popup-20170420.544/magit-popup-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
-;;;;;;  (22721 10691 0 0))
+;;;;;;  (22792 8676 0 0))
 
 ;;;***
 
