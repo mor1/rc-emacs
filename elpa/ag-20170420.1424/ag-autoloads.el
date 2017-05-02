7b2a448c7e5d6d206028437923862693f8e0b5a0
Richard Mortier <mort@cantab.net>
2017-05-02 07:12:42 +0100

    emacs: add `ag`

--- elpa/ag-20170420.1424/ag-autoloads.el
+++ elpa/ag-20170420.1424/ag-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ag" "ag.el" (22636 51207 0 0))
+;;;### (autoloads nil "ag" "ag.el" (22792 8897 0 0))
 ;;; Generated autoloads from ag.el
 
 (autoload 'ag "ag" "\
