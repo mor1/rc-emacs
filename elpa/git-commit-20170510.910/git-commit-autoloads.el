0281dca236b24979041dc83df59dc741d8eb2e55
Richard Mortier <mort@cantab.net>
2017-05-17 00:37:16 +0100

    emacs: add `git-commit`

--- elpa/git-commit-20170510.910/git-commit-autoloads.el
+++ elpa/git-commit-20170510.910/git-commit-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "git-commit" "git-commit.el" (22751 32896 0
+;;;### (autoloads nil "git-commit" "git-commit.el" (22811 31730 0
 ;;;;;;  0))
 ;;; Generated autoloads from git-commit.el
 
@@ -25,6 +25,13 @@ when Git uses the Emacsclient as $GIT_EDITOR to have the user
 provide such a commit message.
 
 \(fn &optional ARG)" t nil)
+
+(defconst git-commit-filename-regexp "/\\(\\(\\(COMMIT\\|NOTES\\|PULLREQ\\|TAG\\)_EDIT\\|MERGE_\\|\\)MSG\\|BRANCH_DESCRIPTION\\)\\'")
+
+(autoload 'git-commit-setup "git-commit" "\
+
+
+\(fn)" nil nil)
 
 ;;;***
 
