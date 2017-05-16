c15cb6cc744cca087f0615de62a9698ef6433cba
Richard Mortier <mort@cantab.net>
2017-05-17 00:37:16 +0100

    emacs: add `tuareg`

--- elpa/tuareg-20170508.930/tuareg-autoloads.el
+++ elpa/tuareg-20170508.930/tuareg-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "ocamldebug" "ocamldebug.el" (22721 10685 0
+;;;### (autoloads nil "ocamldebug" "ocamldebug.el" (22811 31714 0
 ;;;;;;  0))
 ;;; Generated autoloads from ocamldebug.el
 
@@ -19,7 +19,7 @@ the ocamldebug commands `cd DIR' and `directory'.
 
 ;;;***
 
-;;;### (autoloads nil "tuareg" "tuareg.el" (22721 10685 0 0))
+;;;### (autoloads nil "tuareg" "tuareg.el" (22811 31714 0 0))
 ;;; Generated autoloads from tuareg.el
 (add-to-list 'auto-mode-alist '("\\.ml[ip]?\\'" . tuareg-mode))
 (add-to-list 'auto-mode-alist '("\\.eliomi?\\'" . tuareg-mode))
@@ -83,7 +83,7 @@ Run an OCaml toplevel process.  I/O via buffer `*ocaml-toplevel*'.
 ;;;***
 
 ;;;### (autoloads nil nil ("dot-emacs.el" "tuareg-light.el" "tuareg-pkg.el"
-;;;;;;  "tuareg-site-file.el" "tuareg_indent.el") (22721 10685 0
+;;;;;;  "tuareg-site-file.el" "tuareg_indent.el") (22811 31714 0
 ;;;;;;  0))
 
 ;;;***
