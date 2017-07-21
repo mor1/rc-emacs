04fead6d22a66f19cd1cd049677b5f84f2761265
Richard Mortier <mort@cantab.net>
2017-07-21 11:45:33 +0100

    emacs: add `protobuf-mode`

--- elpa/protobuf-mode-20170526.950/protobuf-mode-autoloads.el
+++ elpa/protobuf-mode-20170526.950/protobuf-mode-autoloads.el
@@ -1,9 +1,9 @@
 ;;; protobuf-mode-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "protobuf-mode" "protobuf-mode.el" (22444 32588
+;;;### (autoloads nil "protobuf-mode" "protobuf-mode.el" (22834 52786
 ;;;;;;  0 0))
 ;;; Generated autoloads from protobuf-mode.el
  (add-to-list 'auto-mode-alist '("\\.proto\\'" . protobuf-mode))
