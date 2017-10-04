ad8f9a9a85d3bdd2e61b5c9f217e7272478061f9
Richard Mortier <mort@cantab.net>
2017-10-04 07:42:34 +0100

    emacs: add `bash-completion`

---
+++ elpa/bash-completion-20170924.1021/bash-completion-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "bash-completion" "20170924.1021" "BASH completion for the shell buffer" 'nil :stars '(#("104" 0 3 (font-lock-face paradox-star-face))) :commit "31bc1c1c21691668c6cc16a46361490d5bec303a")
