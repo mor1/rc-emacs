d3f245607167cd25a1ca27bb971447a96e133360
Richard Mortier <mort@cantab.net>
2017-10-04 07:42:35 +0100

    emacs: add `unfill`

---
+++ elpa/unfill-20170722.1846/unfill-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "unfill" "20170722.1846" "Unfill paragraphs or regions, and toggle between filled & unfilled" 'nil :stars '(#("17" 0 2 (font-lock-face paradox-star-face))) :commit "df0c4dee19a3874b11c7c7f04e8a2fba629fda9b" :url "https://github.com/purcell/unfill" :keywords '("utilities"))
