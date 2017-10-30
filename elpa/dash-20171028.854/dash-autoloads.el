b3d02e55b7973f83471a8990173e8cc476b1ae4f
Richard Mortier <mort@cantab.net>
2017-10-30 22:16:40 +0000

    emacs: add `dash`

---
+++ elpa/dash-20171028.854/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("dash.el") (23031 42069 444885 463000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
