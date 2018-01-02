84faf3b008b71c78efa29aec6e84fec2b21c11cc
Richard Mortier <mort@cantab.net>
2018-01-02 21:39:53 +0000

    emacs: add `avy`

--- elpa/avy-20171230.220/avy-autoloads.el
+++ elpa/avy-20171230.220/avy-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "avy" "avy.el" (22996 33429 0 0))
+;;;### (autoloads nil "avy" "avy.el" (23112 54131 51721 700000))
 ;;; Generated autoloads from avy.el
 
 (autoload 'avy-goto-char "avy" "\
