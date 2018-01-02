28c24d4b25226870bd981d6689510713f7604e6d
Richard Mortier <mort@cantab.net>
2018-01-02 21:39:57 +0000

    emacs: remove `gscholar-bibtex`

--- elpa/gscholar-bibtex-20170713.1934/gscholar-bibtex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gscholar-bibtex" "20170713.1934" "Retrieve BibTeX from Google Scholar and other online sources(ACM, IEEE, DBLP)" 'nil :stars '(#("23" 0 2 (font-lock-face paradox-star-face))) :commit "655bccf0b12c9d95ec992ee4bfb5c7c9a4d0c99b" :keywords '("extensions"))
--- elpa/gscholar-bibtex-20170913.1457/gscholar-bibtex-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gscholar-bibtex" "20170913.1457" "Retrieve BibTeX from Google Scholar and other online sources(ACM, IEEE, DBLP)" 'nil :stars '(#("45" 0 2 (font-lock-face paradox-star-face))) :commit "ba4ce159e385d695d8560e8b06b3cbe48424861c" :keywords '("extensions"))
