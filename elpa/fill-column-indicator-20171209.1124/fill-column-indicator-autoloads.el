5525027c95fbee53311ca6b3d7e446b049cd77b9
Richard Mortier <mort@cantab.net>
2018-01-02 21:39:54 +0000

    emacs: add `fill-column-indicator`

--- elpa/fill-column-indicator-20171209.1124/fill-column-indicator-autoloads.el
+++ elpa/fill-column-indicator-20171209.1124/fill-column-indicator-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil "fill-column-indicator" "fill-column-indicator.el"
-;;;;;;  (22996 33420 0 0))
+;;;;;;  (23086 25205 577316 199000))
 ;;; Generated autoloads from fill-column-indicator.el
 
 (autoload 'fci-mode "fill-column-indicator" "\
