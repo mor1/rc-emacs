41405852b39c0478a92ef3b3b91d302a7cb10007
Richard Mortier <mort@cantab.net>
2018-01-02 21:39:56 +0000

    emacs: add `web-mode`

--- elpa/web-mode-20171118.1132/web-mode-autoloads.el
+++ elpa/web-mode-20171118.1132/web-mode-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "web-mode" "web-mode.el" (22885 843 0 0))
+;;;### (autoloads nil "web-mode" "web-mode.el" (23065 55092 450311
+;;;;;;  498000))
 ;;; Generated autoloads from web-mode.el
 
 (autoload 'web-mode "web-mode" "\
