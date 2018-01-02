2938ffeaf75f540ae18843445ba3512a3c197ba9
Richard Mortier <mort@cantab.net>
2018-01-02 21:39:57 +0000

    emacs: add `with-editor`

--- elpa/with-editor-20171127.635/with-editor-autoloads.el
+++ elpa/with-editor-20171127.635/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22885 841 0 0))
+;;;;;;  (23080 7000 157114 132000))
 
 ;;;***
 
--- elpa/with-editor-20171217.1239/with-editor-autoloads.el
+++ elpa/with-editor-20171217.1239/with-editor-autoloads.el
@@ -4,7 +4,7 @@
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
 ;;;### (autoloads nil nil ("with-editor-pkg.el" "with-editor.el")
-;;;;;;  (22996 33376 0 0))
+;;;;;;  (23112 53555 185057 892000))
 
 ;;;***
 
