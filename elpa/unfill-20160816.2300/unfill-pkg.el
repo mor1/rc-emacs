79b349e0f8ddc98df80e7b40d79648922568731e
Richard Mortier <mort@cantab.net>
2018-01-02 21:39:57 +0000

    emacs: remove `unfill`

--- elpa/unfill-20160816.2300/unfill-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "unfill" "20160816.2300" "Unfill paragraphs or regions, and toggle between filled & unfilled" 'nil :stars '(#("6" 0 1 (font-lock-face paradox-star-face))) :url "https://github.com/purcell/unfill" :keywords '("utilities"))
