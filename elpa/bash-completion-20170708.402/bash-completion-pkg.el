ca39859abf778ea26592b0f0a58a856867e149a1
Richard Mortier <mort@cantab.net>
2018-01-02 21:39:57 +0000

    emacs: remove `bash-completion`

--- elpa/bash-completion-20170708.402/bash-completion-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "bash-completion" "20170708.402" "BASH completion for the shell buffer" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- elpa/bash-completion-20170924.1021/bash-completion-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bash-completion" "20170924.1021" "BASH completion for the shell buffer" 'nil :stars '(#("104" 0 3 (font-lock-face paradox-star-face))) :commit "31bc1c1c21691668c6cc16a46361490d5bec303a")
