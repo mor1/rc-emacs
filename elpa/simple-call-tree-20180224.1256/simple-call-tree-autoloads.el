34df7f32b6df1915c4e3d4ffed4391aafb9a005b
Richard Mortier <mort@cantab.net>
2018-03-05 21:37:03 +0000

    emacs: add `simple-call-tree`

---
+++ elpa/simple-call-tree-20180224.1256/simple-call-tree-autoloads.el
@@ -0,0 +1,47 @@
+;;; simple-call-tree-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "simple-call-tree" "simple-call-tree.el" (23197
+;;;;;;  32734 513582 438000))
+;;; Generated autoloads from simple-call-tree.el
+
+(autoload 'simple-call-tree-display-buffer "simple-call-tree" "\
+Display call tree for current buffer.
+If optional arg FILES is supplied it specifies a list of files to search for 
+functions to display in the tree. When called interactively with a prefix arg, 
+files will be prompted for and only functions in the current buffer will be used.
+
+\(fn &optional FILES)" t nil)
+
+(autoload 'simple-call-tree-build-tree "simple-call-tree" "\
+Build the simple-call-tree and display it in the \"*Simple Call Tree*\" buffer.
+If BUFFERS is supplied it should be a list of buffer to analyze, otherwise the buffers
+listed in `simple-call-tree-buffers' will be used.
+
+\(fn &optional BUFFERS)" t nil)
+
+(autoload 'simple-call-tree-current-function "simple-call-tree" "\
+Display call tree for function FUNC.
+If called interactively FUNC will be set to the symbol nearest point,
+unless a prefix arg is used in which case the function returned by `which-function'
+will be used.
+Note: `which-function' may give incorrect results if `imenu' has not been used in
+the current buffer.
+If a call tree containing FUNC has not already been created then the user is prompted
+for which files to build the tree from.
+
+If optional arg WIDE is non-nil then the *Simple Call Tree* buffer will be widened,
+otherwise it will be narrowed around FUNC.
+
+\(fn FUNC &optional WIDE)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; simple-call-tree-autoloads.el ends here
---
+++ elpa/simple-call-tree-20180224.1256/simple-call-tree-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "simple-call-tree" "20180224.1256" "analyze source code based on font-lock text-properties" '((emacs "24.3") (anaphora "1.0.0")) :stars '(#("11" 0 2 (font-lock-face paradox-star-face))) :commit "20059eb5549408def76aeb03d0d20839903dedef" :url "http://www.emacswiki.org/emacs/download/simple-call-tree.el" :keywords '("programming"))
