6ecbe53661002f003a363cf1da65adcf43796868
Richard Mortier <mort@cantab.net>
2018-03-08 17:03:43 +0000

    emacs: add `flycheck`

--- elpa/flycheck-20180308.535/flycheck-autoloads.el
+++ elpa/flycheck-20180308.535/flycheck-autoloads.el
@@ -3,8 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "flycheck" "flycheck.el" (23197 32773 748671
-;;;;;;  154000))
+;;;### (autoloads nil "flycheck" "flycheck.el" (23201 27863 338840
+;;;;;;  249000))
 ;;; Generated autoloads from flycheck.el
 
 (autoload 'flycheck-manual "flycheck" "\
@@ -228,7 +228,7 @@ Use this together with the `option', `option-list' and
 ;;;***
 
 ;;;### (autoloads nil nil ("flycheck-buttercup.el" "flycheck-ert.el"
-;;;;;;  "flycheck-pkg.el") (23197 32773 746783 327000))
+;;;;;;  "flycheck-pkg.el") (23201 27863 336894 495000))
 
 ;;;***
 
