02beb7d3b6e53f549310e7d87cface836f1ce860
Richard Mortier <mort@cantab.net>
2018-04-09 13:57:59 +0100

    emacs: add `go-mode`

--- elpa/go-mode-20180327.830/go-mode-autoloads.el
+++ elpa/go-mode-20180327.830/go-mode-autoloads.el
@@ -3,7 +3,8 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "go-mode" "go-mode.el" (23080 7916 940001 287000))
+;;;### (autoloads nil "go-mode" "go-mode.el" (23242 44495 640119
+;;;;;;  428000))
 ;;; Generated autoloads from go-mode.el
 
 (autoload 'go-mode "go-mode" "\
