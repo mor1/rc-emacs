f105231a93843ab1eb20312cfc8d042312848b22
Richard Mortier <mort@cantab.net>
2018-04-09 13:58:00 +0100

    emacs: add `irony`

--- elpa/irony-20180104.1109/irony-autoloads.el
+++
@@ -1,51 +0,0 @@
-;;; irony-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;***
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil "irony" "irony.el" (23122 39492 935138 9000))
-;;; Generated autoloads from irony.el
-
-;;;### (autoloads nil "irony-cdb" "irony-cdb.el" (23122 39492 853276
-;;;;;;  560000))
-;;; Generated autoloads from irony-cdb.el
-
-;;;### (autoloads nil "irony-cdb-clang-complete" "irony-cdb-clang-complete.el"
-;;;;;;  (23122 39492 932101 933000))
-;;; Generated autoloads from irony-cdb-clang-complete.el
-
-;;;***
-
-;;;### (autoloads nil "irony-cdb-json" "irony-cdb-json.el" (23122
-;;;;;;  39492 933605 829000))
-;;; Generated autoloads from irony-cdb-json.el
-
-;;;***
-
-;;;### (autoloads nil "irony-cdb-libclang" "irony-cdb-libclang.el"
-;;;;;;  (23122 39492 929071 805000))
-;;; Generated autoloads from irony-cdb-libclang.el
-
-;;;***
-
-;;;### (autoloads nil "irony-completion" "irony-completion.el" (23122
-;;;;;;  39492 854925 77000))
-;;; Generated autoloads from irony-completion.el
-
-;;;### (autoloads nil nil ("irony-diagnostics.el" "irony-iotask.el"
-;;;;;;  "irony-pkg.el" "irony-snippet.el") (23122 39492 936780 388000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; irony-autoloads.el ends here
