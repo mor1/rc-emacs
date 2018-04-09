f725d30703d4b6f4f7276fd3579f6eeb4b154168
Richard Mortier <mort@cantab.net>
2018-04-09 13:58:01 +0100

    emacs: remove `e2ansi`

--- elpa/e2ansi-20150220.1513/e2ansi-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "e2ansi" "20150220.1513" "Syntax highlighting support for `less', powered by Emacs." 'nil :url "https://github.com/Lindydancer/e2ansi" :keywords '("faces" "languages"))
