4d235f135e512480979d51841f20cab92f2b141c
Richard Mortier <mort@cantab.net>
2018-04-14 17:52:39 +0100

    emacs: remove `d-mode`

--- elpa/d-mode-20180215.727/d-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "d-mode" "20180215.727" "D Programming Language major mode for (X)Emacs" 'nil :stars '(#("44" 0 2 (font-lock-face paradox-star-face))) :commit "398f1854d53863185e60873b494eec0263bdd0dd" :keywords '("d" "programming" "language" "emacs" "cc-mode"))
