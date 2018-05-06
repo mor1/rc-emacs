ca3f52bc087f17d93d98aa78d15df1edf78052eb
Richard Mortier <mort@cantab.net>
2018-05-06 22:06:43 +0100

    emacs: add `flycheck`

---
+++ elpa/flycheck-lilypond-20171203.532/flycheck-lilypond-autoloads.el
@@ -0,0 +1,16 @@
+;;; flycheck-lilypond-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flycheck-lilypond.el") (23251 45908 61817
+;;;;;;  35000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flycheck-lilypond-autoloads.el ends here
---
+++ elpa/flycheck-mypy-20160220.1432/flycheck-mypy-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "flycheck-mypy" "20160220.1432" "Support mypy in flycheck" '((flycheck "0.18")) :stars '(#("14" 0 2 (font-lock-face paradox-star-face))) :commit "2675dc0e31f8c08eb69f77b8d99e3f7d985c62ee")
