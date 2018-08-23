7244cb7d56560fe082b2a718d58760a79c7f62b6
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:40:54 +0100

    emacs: add `mu4e-maildirs-extension`

--- elpa/mu4e-maildirs-extension-20180606.812/mu4e-maildirs-extension-autoloads.el
+++ elpa/mu4e-maildirs-extension-20180606.812/mu4e-maildirs-extension-autoloads.el
@@ -1,10 +1,13 @@
 ;;; mu4e-maildirs-extension-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "mu4e-maildirs-extension" "mu4e-maildirs-extension.el"
-;;;;;;  (23243 38518 90071 643000))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from mu4e-maildirs-extension.el
 
 (autoload 'mu4e-maildirs-extension-load "mu4e-maildirs-extension" "\
@@ -19,11 +22,14 @@ Un-initialize.
 
 (defalias 'mu4e-maildirs-extension 'mu4e-maildirs-extension-load)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "mu4e-maildirs-extension" '("mu4e-maildirs-extension-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; mu4e-maildirs-extension-autoloads.el ends here
