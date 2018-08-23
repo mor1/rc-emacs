5430c0857ca7f1d440c8c739f60d7b31f7139b74
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:40:45 +0100

    emacs: add `anaphora`

--- elpa/anaphora-20140728.1536/anaphora-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; anaphora-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;***
-
-;;;### (autoloads nil "anaphora" "anaphora.el" (23122 39419 606921
-;;;;;;  144000))
-;;; Generated autoloads from anaphora.el
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; anaphora-autoloads.el ends here
