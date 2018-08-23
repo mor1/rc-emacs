e68eef6868b03065c4cb0dc5310a6a4f29838147
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:40:48 +0100

    emacs: add `epl`

--- elpa/epl-20180205.1249/epl-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; epl-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("epl.el") (23197 32776 12431 816000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; epl-autoloads.el ends here
