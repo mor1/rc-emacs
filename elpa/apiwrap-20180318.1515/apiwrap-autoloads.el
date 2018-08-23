d875e033b3ad4551409c6e301227fa99d955c96f
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:40:46 +0100

    emacs: add `apiwrap`

--- elpa/apiwrap-20180318.1515/apiwrap-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; apiwrap-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("apiwrap.el") (23242 44509 329489 879000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; apiwrap-autoloads.el ends here
