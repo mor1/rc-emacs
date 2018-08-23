b04cc4348c055d4c45c012138b8fd6c0aad99e19
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:40:52 +0100

    emacs: add `latex-pretty-symbols`

--- elpa/latex-pretty-symbols-20151112.244/latex-pretty-symbols-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; latex-pretty-symbols-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("latex-pretty-symbols.el") (22111 13667
-;;;;;;  518999 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; latex-pretty-symbols-autoloads.el ends here
