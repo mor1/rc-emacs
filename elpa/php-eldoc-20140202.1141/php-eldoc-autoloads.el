12277b0f725869ae9c63dea1b70c2581edca2afc
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:40:57 +0100

    emacs: add `php-eldoc`

--- elpa/php-eldoc-20140202.1141/php-eldoc-autoloads.el
+++
@@ -1,27 +0,0 @@
-;;; php-eldoc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "php-eldoc" "php-eldoc.el" (23243 60719 671390
-;;;;;;  907000))
-;;; Generated autoloads from php-eldoc.el
-
-(autoload 'php-eldoc-enable "php-eldoc" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("php-eldoc-pkg.el") (23243 60719 672947
-;;;;;;  394000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; php-eldoc-autoloads.el ends here
