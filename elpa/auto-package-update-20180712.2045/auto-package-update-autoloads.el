c96529dc930a4904f914f41b6beb8ddd158029ed
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:40:46 +0100

    emacs: add `auto-package-update`

--- elpa/auto-package-update-20180712.2045/auto-package-update-autoloads.el
+++ elpa/auto-package-update-20180712.2045/auto-package-update-autoloads.el
@@ -1,10 +1,13 @@
 ;;; auto-package-update-autoloads.el --- automatically extracted autoloads
 ;;
 ;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
 ;;;### (autoloads nil "auto-package-update" "auto-package-update.el"
-;;;;;;  (23296 34872 146621 275000))
+;;;;;;  (0 0 0 0))
 ;;; Generated autoloads from auto-package-update.el
 
 (autoload 'auto-package-update-now "auto-package-update" "\
@@ -22,11 +25,14 @@ Update installed Emacs packages if at least `auto-package-update-interval' days 
 
 \(fn)" nil nil)
 
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "auto-package-update" '("apu--" "auto-package-update-")))
+
 ;;;***
 
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
 ;; no-update-autoloads: t
+;; coding: utf-8
 ;; End:
 ;;; auto-package-update-autoloads.el ends here
