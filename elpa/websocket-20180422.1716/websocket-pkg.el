6c1f43be5c3354d37364cc5d1222771d3790b477
Richard Mortier <richard.mortier@docker.com>
2018-08-23 14:41:00 +0100

    emacs: add `websocket`

--- elpa/websocket-20180422.1716/websocket-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "websocket" "20180422.1716" "Emacs WebSocket client and server" '((cl-lib "0.5")) :stars '(#("193" 0 3 (font-lock-face paradox-star-face))) :commit "0d96ba2ff5a25c6cd6c66f417cc9b5f38a4308ba" :keywords '("communication" "websocket" "server"))
