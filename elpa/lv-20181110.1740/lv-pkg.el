d489458dde5da02370c5d2cbc7c1a9d3d5399e9a
Richard Mortier <mort@cantab.net>
2019-04-15 11:41:49 +0100

    emacs: add `lv`

---
+++ elpa/lv-20181110.1740/lv-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lv" "20181110.1740" "Other echo area" 'nil :stars '(#("982" 0 3 (font-lock-face paradox-star-face))) :commit "acb69859f3dde8d6924587f39997320256bdd85d" :authors '(("Oleh Krehel")) :maintainer '("Oleh Krehel"))
