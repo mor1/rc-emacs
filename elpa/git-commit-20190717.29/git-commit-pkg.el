59555c55bacb0835700f1580298619db04564fd6
Richard Mortier <mort@cantab.net>
2019-08-17 16:53:14 +0100

    emacs: add `git-commit`

---
+++ elpa/git-commit-20190717.29/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20190717.29" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :stars '(#("3932" 0 4 (font-lock-face paradox-star-face))) :commit "7246bf2912a3d1bb3dc7f51a35c7e0365b16f329" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
