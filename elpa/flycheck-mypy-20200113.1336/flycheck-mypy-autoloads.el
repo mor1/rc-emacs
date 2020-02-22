1ecbf2d25998a2ee72f59c87bce9e761b525426c
Richard Mortier <richard.mortier@cdbb.cam.ac.uk>
2020-02-22 09:39:58 +0000

    emacs: add `flycheck-mypy`

--- elpa/flycheck-mypy-20200113.1336/flycheck-mypy-autoloads.el
+++ elpa/flycheck-mypy-20200113.1336/flycheck-mypy-autoloads.el
@@ -6,6 +6,13 @@
                          (or (file-name-directory #$) (car load-path))))
 
 
+;;;### (autoloads nil "flycheck-mypy" "flycheck-mypy.el" (0 0 0 0))
+;;; Generated autoloads from flycheck-mypy.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "flycheck-mypy" '("flycheck-mypy--find-project-root")))
+
+;;;***
+
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
