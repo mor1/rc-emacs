108f5c290f4c0082f7b08132819d50d568f4833e
Richard Mortier <mort@cantab.net>
2020-04-09 17:56:53 +0100

    emacs: add `php-mode`

--- elpa/php-mode-20200402.326/php-mode-autoloads.el
+++ elpa/php-mode-20200402.326/php-mode-autoloads.el
@@ -52,6 +52,23 @@ Find php.ini FILE by `php --ini'.
 
 ;;;***
 
+;;;### (autoloads nil "php-align" "php-align.el" (0 0 0 0))
+;;; Generated autoloads from php-align.el
+
+(autoload 'php-align-setup "php-align" "\
+Setup alignment configuration for PHP code.
+
+\(fn)" nil nil)
+
+(autoload 'php-align-mode "php-align" "\
+Alignment lines for PHP script.
+
+\(fn &optional ARG)" t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "php-align" '("php-align-")))
+
+;;;***
+
 ;;;### (autoloads nil "php-face" "php-face.el" (0 0 0 0))
 ;;; Generated autoloads from php-face.el
 
@@ -142,6 +159,11 @@ Symbol value of the coding style of the project that PHP major mode refers to.
 Typically it is `pear', `drupal', `wordpress', `symfony2' and `psr2'.")
 
 (put 'php-project-coding-style 'safe-local-variable #'symbolp)
+
+(defvar-local php-project-align-lines t "\
+If T, automatically turn on `php-align-mode' by `php-align-setup'.")
+
+(put 'php-project-align-lines 'safe-local-variable #'booleanp)
 
 (defvar-local php-project-php-file-as-template 'auto "\
 
--- elpa/php-mode-20200402.326/php-mode.el
+++ elpa/php-mode-20200402.326/php-mode.el
@@ -1494,6 +1494,15 @@ a completion list."
    ;;  only add patterns here if you want to prevent cc-mode from applying
    ;;  a different face.
    `(
+     ;; Class declaration specification keywords (implements, extends)
+     ("\\_<\\(?:implements\\|extends\\)\\_>" . 'php-class-declaration-spec)
+     ;; Namespace declaration
+     ("\\_<namespace\\_>" . 'php-namespace-declaration)
+     ;; import statement
+     ("\\_<use\\_>" . 'php-import-declaration)
+     ;; Class modifiers (abstract, final)
+     ("\\_<\\(abstract\\|final\\)\\_>\\s-+\\_<class\\>" 1 'php-class-modifier)
+
      ;; Highlight variables, e.g. 'var' in '$var' and '$obj->var', but
      ;; not in $obj->var()
      ("\\(->\\)\\(\\sw+\\)\\s-*(" (1 'php-object-op) (2 'php-method-call))
@@ -1533,6 +1542,8 @@ a completion list."
       1 'php-class)
      ;; Support the ::class constant in PHP5.6
      ("\\sw+\\(::\\)\\(class\\)\\b" (1 'php-paamayim-nekudotayim) (2 'php-magical-constant))
+     ;; Class declaration keywords (class, trait, interface)
+     ("\\_<\\(class\\|trait\\|interface\\)\\_>" . 'php-class-declaration)
 
      ;; Highlight static method calls as such. This is necessary for method
      ;; names which are identical to keywords to be highlighted correctly.
