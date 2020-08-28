b99ef1c92fee6a6149eb65b1f5607b66a3873a24
Richard Mortier <mort@cantab.net>
2020-08-28 12:22:11 +0100

    emacs: add `fill-column-indicator`

--- elpa/fill-column-indicator-20171209.1924/fill-column-indicator-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "fill-column-indicator" "20171209.1924" "Graphically indicate the fill column" 'nil :stars '(#("180" 0 3 (font-lock-face paradox-star-face))) :commit "d2536b1c48f78679e15a2b50cd5d8c0ffde4b155" :keywords '("convenience") :authors '(("Alp Aker" . "alp.tekin.aker@gmail.com")) :maintainer '("Alp Aker" . "alp.tekin.aker@gmail.com"))
