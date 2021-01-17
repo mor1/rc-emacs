128c5592c3a20b073f3e673ebd040a216f23a24f
Richard Mortier <mort@cantab.net>
2021-01-17 19:15:46 +0000

    emacs: add `org`

---
+++ elpa/org-clock-today-20191204.1558/org-clock-today-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/mort/.emacs.d/elpa/org-clock-today-20191204.1558/org-clock-today.el  -*- no-byte-compile: t -*-
+(define-package "org-clock-today" "20191204.1558" "Show total clocked time of the current day in the mode line" '((emacs "25")) :stars '(#("8" 0 1 (font-lock-face paradox-star-face))) :commit "e326a45b60e0fd4ca057f1d1dc3e99a516a5aa2f" :authors '(("Tijs Mallaerts" . "tijs.mallaerts@gmail.com")) :maintainer '("Tijs Mallaerts" . "tijs.mallaerts@gmail.com") :url "https://github.com/mallt/org-clock-today-mode")
