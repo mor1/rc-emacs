922492c7adfb9b7bd6b810a4d075b18ae57b598a
Richard Mortier <mort@cantab.net>
2021-01-17 19:15:45 +0000

    emacs: add `json-snatcher`

---
+++ elpa/json-snatcher-20200916.1717/json-snatcher-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/mort/.emacs.d/elpa/json-snatcher-20200916.1717/json-snatcher.el  -*- no-byte-compile: t -*-
+(define-package "json-snatcher" "20200916.1717" "Grabs the path to JSON values in a JSON file" '((emacs "24")) :stars '(#("60" 0 2 (font-lock-face paradox-star-face))) :commit "b28d1c0670636da6db508d03872d96ffddbc10f2" :authors '(("Sterling Graham" . "sterlingrgraham@gmail.com")) :maintainer '("Sterling Graham" . "sterlingrgraham@gmail.com") :url "http://github.com/sterlingg/json-snatcher")
