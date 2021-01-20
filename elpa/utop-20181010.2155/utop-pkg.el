df24effcd1ac1430e653d0caf99daee999510ce9
Richard Mortier <mort@cantab.net>
2021-01-20 00:18:54 +0000

    emacs: remove `utop`

--- elpa/utop-20181010.2155/utop-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "utop" "20181010.2155" "Universal toplevel for OCaml" '((emacs "24")) :stars '(#("434" 0 3 (font-lock-face paradox-star-face))) :commit "ea38850e606dd18c94e2ccabc28485fec1c8f91f" :keywords '("ocaml" "languages") :authors '(("Jeremie Dimino" . "jeremie@dimino.org")) :maintainer '("Jeremie Dimino" . "jeremie@dimino.org") :url "https://github.com/diml/utop")
