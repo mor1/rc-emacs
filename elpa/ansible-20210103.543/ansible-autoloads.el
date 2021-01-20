1db8320ddb3f8ce348d0099edadb2ff41beb1486
Richard Mortier <mort@cantab.net>
2021-01-20 00:18:47 +0000

    emacs: add `ansible`

--- elpa/ansible-20210103.543/ansible-autoloads.el
+++ elpa/ansible-20210103.543/ansible-autoloads.el
@@ -15,12 +15,15 @@ Keymap for Ansible.")
 (autoload 'ansible "ansible" "\
 Ansible minor mode.
 
+If called interactively, enable Ansible mode if ARG is positive,
+and disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it if
+ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
 
 (autoload 'ansible-dict-initialize "ansible" "\
-Initialize Ansible auto-complete.
-
-\(fn)" nil nil)
+Initialize Ansible auto-complete." nil nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ansible" '("ansible-")))
 
