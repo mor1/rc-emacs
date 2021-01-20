1a986b22d1d4285ea415146aa59643731b9a3b9e
Richard Mortier <mort@cantab.net>
2021-01-20 00:18:53 +0000

    emacs: add `visual-fill-column`

--- elpa/visual-fill-column-20201229.2303/visual-fill-column-autoloads.el
+++ elpa/visual-fill-column-20201229.2303/visual-fill-column-autoloads.el
@@ -13,7 +13,14 @@
 (autoload 'visual-fill-column-mode "visual-fill-column" "\
 Wrap lines according to `fill-column' in `visual-line-mode'.
 
+If called interactively, enable Visual-Fill-Column mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
+
+(put 'global-visual-fill-column-mode 'globalized-minor-mode t)
 
 (defvar global-visual-fill-column-mode nil "\
 Non-nil if Global Visual-Fill-Column mode is enabled.
@@ -42,15 +49,15 @@ Split WINDOW sensibly, unsetting its margins first.
 This function unsets the window margins and calls
 `split-window-sensibly'.
 
-By default, `split-window-sensibly' does not split a window
-vertically if it has wide margins, even if there is enough space
-for a vertical split.  This function can be used as the value of
-`split-window-preferred-function' to enable vertically splitting
-windows with wide margins.
+By default, `split-window-sensibly' does not split a window in
+two side-by-side windows if it has wide margins, even if there is
+enough space for a vertical split.  This function is used as the
+value of `split-window-preferred-function' to allow
+`display-buffer' to split such windows.
 
 \(fn &optional WINDOW)" nil nil)
 
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "visual-fill-column" '("visual-fill-column-" "turn-on-visual-fill-column-mode")))
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "visual-fill-column" '("turn-on-visual-fill-column-mode" "visual-fill-column-")))
 
 ;;;***
 
