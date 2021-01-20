a305a4168713a782aa82ffd8d6711d8350a85fb8
Richard Mortier <mort@cantab.net>
2021-01-20 00:18:54 +0000

    emacs: remove `syslog-mode`

--- elpa/syslog-mode-20170107.1517/syslog-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "syslog-mode" "20170107.1517" "Major-mode for viewing log files" '((hide-lines "20130623")) :stars '(#("11" 0 2 (font-lock-face paradox-star-face))) :url "https://github.com/vapniks/syslog-mode" :keywords '("unix"))
--- elpa/syslog-mode-20190913.2040/syslog-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "syslog-mode" "20190913.2040" "Major-mode for viewing log files" '((hide-lines "20130623") (ov "20150311")) :stars '("") :commit "18f441bf57dd70cdd48a71f1f4566ab35facdb35" :keywords '("unix") :authors '(("Harley Gorrell" . "harley@panix.com")) :maintainer '("Joe Bloggs" . "vapniks@yahoo.com") :url "https://github.com/vapniks/syslog-mode")
