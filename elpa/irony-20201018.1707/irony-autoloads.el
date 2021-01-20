22c09bf58947b97f10d22daf1c27172bb37d76f8
Richard Mortier <mort@cantab.net>
2021-01-20 00:18:50 +0000

    emacs: add `irony`

--- elpa/irony-20201018.1707/irony-autoloads.el
+++ elpa/irony-20201018.1707/irony-autoloads.el
@@ -24,6 +24,11 @@ database.")
 (autoload 'irony-mode "irony" "\
 Minor mode for C, C++ and Objective-C, powered by libclang.
 
+If called interactively, enable Irony mode if ARG is positive,
+and disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it if
+ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
 
 (autoload 'irony-version "irony" "\
@@ -34,14 +39,10 @@ If called interactively display the version in the echo area.
 \(fn &optional SHOW-VERSION)" t nil)
 
 (autoload 'irony-server-kill "irony" "\
-Kill the running irony-server process, if any.
-
-\(fn)" t nil)
+Kill the running irony-server process, if any." t nil)
 
 (autoload 'irony-get-type "irony" "\
-Get the type of symbol under cursor.
-
-\(fn)" t nil)
+Get the type of symbol under cursor." t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "irony" '("irony-")))
 
@@ -50,15 +51,9 @@ Get the type of symbol under cursor.
 ;;;### (autoloads nil "irony-cdb" "irony-cdb.el" (0 0 0 0))
 ;;; Generated autoloads from irony-cdb.el
 
-(autoload 'irony-cdb-autosetup-compile-options "irony-cdb" "\
+(autoload 'irony-cdb-autosetup-compile-options "irony-cdb" nil t nil)
 
-
-\(fn)" t nil)
-
-(autoload 'irony-cdb-menu "irony-cdb" "\
-
-
-\(fn)" t nil)
+(autoload 'irony-cdb-menu "irony-cdb" nil t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "irony-cdb" '("irony-cdb-")))
 
@@ -107,14 +102,10 @@ root.
 The completion function used internally is `completing-read' so
 it could easily be used with other completion functions by
 temporarily using a let-bind on `completing-read-function'. Or
-even helm by enabling `helm-mode' before calling the function.
-
-\(fn)" t nil)
+even helm by enabling `helm-mode' before calling the function." t nil)
 
 (autoload 'irony-cdb-json-select-most-recent "irony-cdb-json" "\
-Select CDB that is most recently modified.
-
-\(fn)" t nil)
+Select CDB that is most recently modified." t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "irony-cdb-json" '("irony-cdb-json--")))
 
@@ -137,10 +128,7 @@ Select CDB that is most recently modified.
 ;;;;;;  0 0 0))
 ;;; Generated autoloads from irony-completion.el
 
-(autoload 'irony-completion-at-point "irony-completion" "\
-
-
-\(fn)" nil nil)
+(autoload 'irony-completion-at-point "irony-completion" nil nil nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "irony-completion" '("irony-")))
 
