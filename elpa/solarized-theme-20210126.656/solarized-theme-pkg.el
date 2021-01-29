b9c8762a5d4c45600c8e8e648c0ca3c8d47c1d4c
Richard Mortier <mort@cantab.net>
2021-01-29 23:39:15 +0000

    emacs: add `solarized-theme`

--- elpa/solarized-theme-20210126.656/solarized-theme-pkg.el
+++ elpa/solarized-theme-20210126.656/solarized-theme-pkg.el
@@ -1,7 +1,7 @@
-(define-package "solarized-theme" "20210122.902" "The Solarized color theme"
+(define-package "solarized-theme" "20210126.656" "The Solarized color theme"
   '((emacs "24.1")
     (dash "2.16"))
-  :commit "4a82558a3f29f37441c9d45ffad6d9f40c7f79e5" :authors
+  :commit "98ece6811b30c8ef2ece670b47558a9159f973f1" :authors
   '(("Bozhidar Batsov" . "bozhidar@batsov.com"))
   :maintainer
   '("Bozhidar Batsov" . "bozhidar@batsov.com")
