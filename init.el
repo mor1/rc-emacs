79f934b636428beda2352d45d19f615236274438
Richard Mortier <mort@cantab.net>
2021-03-28 10:51:15 +0100

    js: javascript and typescript modes
    Signed-off-by: Richard Mortier <mort@cantab.net>

--- init.el
+++ init.el
@@ -214,8 +214,14 @@
             )
   )
 
-(use-package js2-mode
-  :mode "\\.js$"
+(use-package typescript-mode
+  :mode ("\\.ts$")
+  :config
+  (setq typescript-indent-level 2)
+  )
+
+(use-package rjsx-mode
+  :mode ("\\.js$")
   :config
   (setq js-indent-level 2)
   )
