6b3384512b144fc392a95f8a7f38bca57bd813dd
Richard Mortier <mort@cantab.net>
2021-09-10 21:12:40 +0100

    emacs: add `php-mode`

--- elpa/php-mode-20210808.1745/php-mode-pkg.el
+++ elpa/php-mode-20210808.1745/php-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "php-mode" "20210310.1724" "Major mode for editing PHP code"
+(define-package "php-mode" "20210808.1745" "Major mode for editing PHP code"
   '((emacs "25.2"))
-  :commit "a2bca9be4c34a9dc38393602cb2708df24587838" :authors
+  :commit "8212f415fc5de2dedfc74d797b3951b2cbfb6f58" :authors
   '(("Eric James Michael Ritz"))
   :maintainer
   '("USAMI Kenta" . "tadsan@zonu.me")
