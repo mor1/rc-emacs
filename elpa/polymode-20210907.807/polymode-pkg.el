96c5d2c377b5779c84d0d43c02122fa0f6379812
Richard Mortier <mort@cantab.net>
2021-09-10 21:34:27 +0100

    emacs: add `polymode`

--- elpa/polymode-20210907.807/polymode-pkg.el
+++ elpa/polymode-20210907.807/polymode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "polymode" "20210521.1131" "Extensible framework for multiple major modes"
+(define-package "polymode" "20210907.807" "Extensible framework for multiple major modes"
   '((emacs "25"))
-  :commit "7d1f822f0833b43326cc9253dc8a3e267ad4b376" :authors
+  :commit "54888d6c15249503e1a66da7bd7761a9eda9b075" :authors
   '(("Vitalie Spinu"))
   :maintainer
   '("Vitalie Spinu")
