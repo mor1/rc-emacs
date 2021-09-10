ded03bc500326b9b72a073eadb67a7e181077cf7
Richard Mortier <mort@cantab.net>
2021-09-10 21:12:41 +0100

    emacs: add `vline`

--- elpa/vline-20210805.1528/vline-autoloads.el
+++ elpa/vline-20210805.1528/vline-autoloads.el
@@ -2,19 +2,29 @@
 ;;
 ;;; Code:
 
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads (vline-global-mode vline-mode) "vline" "vline.el"
-;;;;;;  (21508 15978 0 0))
+;;;### (autoloads nil "vline" "vline.el" (0 0 0 0))
 ;;; Generated autoloads from vline.el
 
 (autoload 'vline-mode "vline" "\
 Display vertical line mode.
 
+If called interactively, enable Vline mode if ARG is positive,
+and disable it if ARG is zero or negative.  If called from Lisp,
+also enable the mode if ARG is omitted or nil, and toggle it if
+ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
+
+(put 'vline-global-mode 'globalized-minor-mode t)
 
 (defvar vline-global-mode nil "\
 Non-nil if Vline-Global mode is enabled.
-See the command `vline-global-mode' for a description of this minor mode.
+See the `vline-global-mode' command
+for a description of this minor mode.
 Setting this variable directly does not take effect;
 either customize it (see the info node `Easy Customization')
 or call the function `vline-global-mode'.")
@@ -33,13 +43,10 @@ See `vline-mode' for more information on Vline mode.
 
 \(fn &optional ARG)" t nil)
 
-;;;***
-
-;;;### (autoloads nil nil ("vline-pkg.el") (21508 15980 186524 0))
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "vline" '("vline-")))
 
 ;;;***
 
-(provide 'vline-autoloads)
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
