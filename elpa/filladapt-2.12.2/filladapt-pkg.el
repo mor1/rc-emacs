3108babf955c869014bf78f8a92930856bd98ed3
Richard Mortier <mort@cantab.net>
2021-09-10 21:34:26 +0100

    emacs: add `filladapt`

---
+++ elpa/filladapt-2.12.2/filladapt-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from filladapt.el  -*- no-byte-compile: t -*-
+(define-package "filladapt" "2.12.2" "Adaptive fill" '((emacs "24.4")) :stars '("") :url "http://elpa.gnu.org/packages/filladapt.html" :authors '(("Kyle E. Jones" . "kyle_jones@wonderworks.com")) :maintainer '(nil . "emacs-devel@gnu.org"))
