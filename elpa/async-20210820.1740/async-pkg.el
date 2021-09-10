717e12f5487d29f46ab0f7a5c4b77f354a7b90de
Richard Mortier <mort@cantab.net>
2021-09-10 21:12:36 +0100

    emacs: add `async`

--- elpa/async-20210820.1740/async-pkg.el
+++ elpa/async-20210820.1740/async-pkg.el
@@ -1,11 +1,11 @@
-(define-package "async" "20210117.718" "Asynchronous processing in Emacs"
-  '((emacs "24.3"))
-  :commit "d7e7f79ee42311a0187aa2ab4f4e2f8843fa28da" :authors
+(define-package "async" "20210820.1740" "Asynchronous processing in Emacs"
+  '((emacs "24.4"))
+  :commit "180742ec4a18107df65efca246e67d698ed23e79" :authors
   '(("John Wiegley" . "jwiegley@gmail.com"))
   :maintainer
   '("John Wiegley" . "jwiegley@gmail.com")
   :keywords
-  '("convenience" "async")
+  '("async")
   :url "https://github.com/jwiegley/emacs-async")
 ;; Local Variables:
 ;; no-byte-compile: t
