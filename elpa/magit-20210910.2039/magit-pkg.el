297b2c7669341752eebf18617e1fb8adf35218b4
Richard Mortier <mort@cantab.net>
2021-09-11 17:04:46 +0100

    emacs: add `magit`

--- elpa/magit-20210910.2039/magit-pkg.el
+++ elpa/magit-20210910.2039/magit-pkg.el
@@ -1,11 +1,11 @@
-(define-package "magit" "20210910.1028" "A Git porcelain inside Emacs."
+(define-package "magit" "20210910.2039" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (dash "20210330")
     (git-commit "20210806")
     (magit-section "20210806")
     (transient "20210701")
     (with-editor "20210524"))
-  :commit "73530108957c1167e155df9658d843c3bac0d9c7" :authors
+  :commit "8771401d4fd2b01951765be4b40ab0b9a89eacb9" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
