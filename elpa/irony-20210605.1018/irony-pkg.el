cc162f197cc32d29c702e54b391e5956af37b85b
Richard Mortier <mort@cantab.net>
2021-09-18 21:00:09 +0100

    emacs: add `irony`

--- elpa/irony-20210605.1018/irony-pkg.el
+++ elpa/irony-20210605.1018/irony-pkg.el
@@ -1,7 +1,7 @@
-(define-package "irony" "20210321.1750" "C/C++ minor mode powered by libclang"
+(define-package "irony" "20210605.1018" "C/C++ minor mode powered by libclang"
   '((cl-lib "0.5")
     (json "1.2"))
-  :commit "ec6dce7ee16ffaa9a735204534aa4aa074d14487" :authors
+  :commit "b9c64abf81e73860e39ecd82dfa00cca90b53d99" :authors
   '(("Guillaume Papin" . "guillaume.papin@epitech.eu"))
   :maintainer
   '("Guillaume Papin" . "guillaume.papin@epitech.eu")
