4dea9d7324e399778b873a49f488a5170b94a1a8
Richard Mortier <mort@ikva.ai>
2022-01-04 10:33:00 +0000

    emacs: add `dockerfile-mode`

--- elpa/dockerfile-mode-20210828.1805/dockerfile-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from dockerfile-mode.el  -*- no-byte-compile: t -*-
-(define-package "dockerfile-mode" "20210828.1805" "Major mode for editing Docker's Dockerfiles" '((emacs "24")) :stars '(#("318" 0 3 (font-lock-face paradox-star-face))) :commit "628315e2e4ab2f269548126444234caa057b2c75" :keywords '("docker") :url "https://github.com/spotify/dockerfile-mode")
