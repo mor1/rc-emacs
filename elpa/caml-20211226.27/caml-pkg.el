c8424834b086ba3fd4389389fd2789b95c13d251
Richard Mortier <mort@ikva.ai>
2022-01-04 10:33:00 +0000

    emacs: add `caml`

--- elpa/caml-20211226.27/caml-pkg.el
+++ elpa/caml-20211226.27/caml-pkg.el
@@ -1,6 +1,6 @@
-(define-package "caml" "20210907.2124" "Caml mode for GNU Emacs"
+(define-package "caml" "20211226.27" "Caml mode for GNU Emacs"
   '((emacs "24.3"))
-  :commit "2905a436e956c5bba16c4633a6e4c4fceefa6535" :authors
+  :commit "204bfde9ad21cb94c273db8b0c12d31a4eb425e5" :authors
   '(("Jacques Garrigue" . "garrigue@kurims.kyoto-u.ac.jp")
     ("Ian T Zimmerman" . "itz@rahul.net")
     ("Damien Doligez" . "damien.doligez@inria.fr"))
