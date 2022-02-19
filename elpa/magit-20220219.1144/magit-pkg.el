fc8f29ff7bc9ae1915f19dcaddc36d01f0212782
Richard Mortier <mort@ikva.ai>
2022-02-19 21:08:51 +0000

    emacs: add `magit`

---
+++ elpa/magit-20220219.1144/magit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "magit" "20220219.1144" "A Git porcelain inside Emacs."
+  '((emacs "25.1")
+    (dash "20210826")
+    (git-commit "20211004")
+    (magit-section "20211004")
+    (transient "20210920")
+    (with-editor "20211001"))
+  :commit "991de20f76644bde6fc653bfad294acb82167cbc" :authors
+  '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+    ("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-section-20220216.1853/magit-section-pkg.el
@@ -0,0 +1,13 @@
+(define-package "magit-section" "20220216.1853" "Sections for read-only buffers"
+  '((emacs "25.1")
+    (dash "20210826"))
+  :commit "991de20f76644bde6fc653bfad294acb82167cbc" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
