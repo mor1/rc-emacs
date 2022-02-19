d2c9658196861426ef16bb4b2e0596a70627d3f8
Richard Mortier <mort@ikva.ai>
2022-02-19 21:08:48 +0000

    emacs: add `async`

--- elpa/async-20220104.1222/async-pkg.el
+++ elpa/async-20220104.1222/async-pkg.el
@@ -1,6 +1,6 @@
-(define-package "async" "20210823.528" "Asynchronous processing in Emacs"
+(define-package "async" "20220104.1222" "Asynchronous processing in Emacs"
   '((emacs "24.4"))
-  :commit "5d365ffc6a2c2041657eaa5d762c395ea748c8d7" :authors
+  :commit "0d52411d3accc3e11a2c64838703a8ce9755c77c" :authors
   '(("John Wiegley" . "jwiegley@gmail.com"))
   :maintainer
   '("John Wiegley" . "jwiegley@gmail.com")
