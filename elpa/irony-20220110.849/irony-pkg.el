33213f333cc155b4545541a9efcf790824886091
Richard Mortier <mort@ikva.ai>
2022-02-19 21:08:50 +0000

    emacs: add `irony`

--- elpa/irony-20220110.849/irony-pkg.el
+++ elpa/irony-20220110.849/irony-pkg.el
@@ -1,7 +1,7 @@
-(define-package "irony" "20210605.1018" "C/C++ minor mode powered by libclang"
+(define-package "irony" "20220110.849" "C/C++ minor mode powered by libclang"
   '((cl-lib "0.5")
     (json "1.2"))
-  :commit "b9c64abf81e73860e39ecd82dfa00cca90b53d99" :authors
+  :commit "870d1576fb279bb93f776a71e65f45283c423a9e" :authors
   '(("Guillaume Papin" . "guillaume.papin@epitech.eu"))
   :maintainer
   '("Guillaume Papin" . "guillaume.papin@epitech.eu")
