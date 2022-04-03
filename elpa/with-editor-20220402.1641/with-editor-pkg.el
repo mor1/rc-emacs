6f924d7cfe4dec80b7303425703d1b2d0a7cc46e
Richard Mortier <mort@cantab.net>
2022-04-03 20:39:26 +0100

    emacs: add `with-editor`

--- elpa/with-editor-20220402.1641/with-editor-pkg.el
+++ elpa/with-editor-20220402.1641/with-editor-pkg.el
@@ -1,6 +1,6 @@
-(define-package "with-editor" "20220211.2034" "Use the Emacsclient as $EDITOR"
+(define-package "with-editor" "20220402.1641" "Use the Emacsclient as $EDITOR"
   '((emacs "24.4"))
-  :commit "f514f23258af67a10fc8e1c431bfe94702b6e65b" :authors
+  :commit "7381fa1ebf9295f2cd6af1f3f25e4adaccbff0bd" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
