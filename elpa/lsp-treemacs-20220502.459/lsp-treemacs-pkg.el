40e5741e01f1ff3bd0e08368f2ba7abc918671f7
Richard Mortier <mort@cantab.net>
2022-05-21 10:26:35 +0100

    emacs: add `lsp-treemacs`

--- elpa/lsp-treemacs-20220502.459/lsp-treemacs-pkg.el
+++ elpa/lsp-treemacs-20220502.459/lsp-treemacs-pkg.el
@@ -1,11 +1,11 @@
-(define-package "lsp-treemacs" "20220328.625" "LSP treemacs"
+(define-package "lsp-treemacs" "20220502.459" "LSP treemacs"
   '((emacs "26.1")
     (dash "2.18.0")
     (f "0.20.0")
     (ht "2.0")
     (treemacs "2.5")
     (lsp-mode "6.0"))
-  :commit "355e468b7fa9887c616a8bfe873d8e456303b67b" :authors
+  :commit "9859326df6b8e8c954a3c227e53b6878e54aaae8" :authors
   '(("Ivan Yonchovski"))
   :maintainer
   '("Ivan Yonchovski")
