c768b94efe07160cd9dcca8729c32ac3f87de76d
Richard Mortier <mort@ikva.ai>
2022-06-18 14:39:31 +0100

    emacs: add `dap-mode`

--- elpa/dap-mode-20220612.1850/dap-mode-pkg.el
+++ elpa/dap-mode-20220612.1850/dap-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dap-mode" "20220518.629" "Debug Adapter Protocol mode"
+(define-package "dap-mode" "20220612.1850" "Debug Adapter Protocol mode"
   '((emacs "26.1")
     (dash "2.18.0")
     (lsp-mode "6.0")
@@ -8,7 +8,7 @@
     (lsp-treemacs "0.1")
     (posframe "0.7.0")
     (ht "2.3"))
-  :commit "67fd9e5d4e157e72b09ab34daca67619cb98c673" :authors
+  :commit "50c2a99059da03ac5274d6794ea4758ddf253dc1" :authors
   '(("Ivan Yonchovski" . "yyoncho@gmail.com"))
   :maintainer
   '("Ivan Yonchovski" . "yyoncho@gmail.com")
