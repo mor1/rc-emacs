7faeb5c3921afd7e408597f820884442fa54c0b1
Richard Mortier <mort@ikva.ai>
2022-06-18 14:39:31 +0100

    emacs: add `ghub`

--- elpa/ghub-20220613.2208/ghub-pkg.el
+++ elpa/ghub-20220613.2208/ghub-pkg.el
@@ -1,9 +1,9 @@
-(define-package "ghub" "20220429.1708" "Client libraries for Git forge APIs."
+(define-package "ghub" "20220613.2208" "Client libraries for Git forge APIs."
   '((emacs "25.1")
     (compat "28.1.1.0")
     (let-alist "1.0.6")
     (treepy "0.1.1"))
-  :commit "f14c1bc84135fe149c42348dce6eaa23ba610224" :authors
+  :commit "7beed8c5c638ed9a94cb5962587ea286f07b8981" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
