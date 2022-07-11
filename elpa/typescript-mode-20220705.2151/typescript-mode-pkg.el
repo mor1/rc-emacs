c9fd804ab9e298ee4089fe4c916bf2caaf9f989d
Richard Mortier <mort@cantab.net>
2022-07-11 14:43:08 +0100

    emacs: add `typescript-mode`

--- elpa/typescript-mode-20220705.2151/typescript-mode-pkg.el
+++ elpa/typescript-mode-20220705.2151/typescript-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "typescript-mode" "20220617.1901" "Major mode for editing typescript"
+(define-package "typescript-mode" "20220705.2151" "Major mode for editing typescript"
   '((emacs "24.3"))
-  :commit "f2de3f4946c846a23753b351eaf8b56017e02d89" :keywords
+  :commit "83bf47e406761d951bd9eab2f0674e4fa3874a1b" :keywords
   '("typescript" "languages")
   :url "http://github.com/ananthakumaran/typescript.el")
 ;; Local Variables:
