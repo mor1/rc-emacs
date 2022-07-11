8cc778ac9fb224690acf4bcd5fd05f76622e86dd
Richard Mortier <mort@cantab.net>
2022-07-11 14:43:07 +0100

    emacs: add `lsp-ui`

--- elpa/lsp-ui-20220707.1336/lsp-ui-pkg.el
+++ elpa/lsp-ui-20220707.1336/lsp-ui-pkg.el
@@ -1,9 +1,9 @@
-(define-package "lsp-ui" "20220514.2010" "UI modules for lsp-mode"
+(define-package "lsp-ui" "20220707.1336" "UI modules for lsp-mode"
   '((emacs "26.1")
     (dash "2.18.0")
     (lsp-mode "6.0")
     (markdown-mode "2.3"))
-  :commit "370022b6785e5c1906cf23fe92d658f10623f3ba" :authors
+  :commit "a94bcec2071ee196bc83e52ad92ec0881f8bcfbd" :authors
   '(("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me"))
   :maintainer
   '("Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song" . "i@maskray.me")
