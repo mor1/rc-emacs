47b84c6f8414ed66edab0c0f43e6b1fef3fd6062
Richard Mortier <mort@cantab.net>
2022-07-11 14:43:05 +0100

    emacs: add `clojure-mode`

---
+++ elpa/clojure-mode-20220706.1132/clojure-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from clojure-mode.el  -*- no-byte-compile: t -*-
+(define-package "clojure-mode" "20220706.1132" "Major mode for Clojure code" '((emacs "25.1")) :stars '(#("695" 0 3 (font-lock-face paradox-star-face))) :commit "d82417cf86644a4135c6d764aa901f69045fd5ca" :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
