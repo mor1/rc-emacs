8a09fd8abe2ff4cf8549cb55fed6d6f4b5161efd
Richard Mortier <mort@cantab.net>
2022-08-24 09:29:28 +0100

    emacs: add `magit`

--- elpa/magit-20220821.1819/magit-pkg.el
+++ elpa/magit-20220821.1819/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20220706.148" "A Git porcelain inside Emacs."
+(define-package "magit" "20220821.1819" "A Git porcelain inside Emacs."
   '((emacs "25.1")
     (compat "28.1.1.2")
     (dash "20210826")
@@ -6,7 +6,7 @@
     (magit-section "20220325")
     (transient "20220325")
     (with-editor "20220318"))
-  :commit "acd26dd9f3708602d4c721395d790a4af7937eed" :authors
+  :commit "712be4632b0ddc7899ca90db8f9be20d90b4326f" :authors
   '(("Marius Vollmer" . "marius.vollmer@gmail.com")
     ("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
--- elpa/magit-section-20220810.1158/magit-section-pkg.el
+++ elpa/magit-section-20220810.1158/magit-section-pkg.el
@@ -1,8 +1,8 @@
-(define-package "magit-section" "20220615.1159" "Sections for read-only buffers."
+(define-package "magit-section" "20220810.1158" "Sections for read-only buffers."
   '((emacs "25.1")
     (compat "28.1.1.2")
     (dash "20210826"))
-  :commit "c1fb53d3de6390961ccd8dfb1cc135383508d0fc" :authors
+  :commit "712be4632b0ddc7899ca90db8f9be20d90b4326f" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
