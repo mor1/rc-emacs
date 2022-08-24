47fddf5f8bedc62ba5fdef334eb88ed9dc307577
Richard Mortier <mort@cantab.net>
2022-08-24 09:29:27 +0100

    emacs: add `helm`

---
+++ elpa/helm-core-20220823.1840/helm-core-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm-core" "20220823.1840" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "26313b28d1c57ac0b46fec4fbf9741358050da03" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
