ac8984848eb0e7ffd442798b67909edad693cadd
Richard Mortier <mort@cantab.net>
2022-09-21 22:47:42 +0100

    emacs: add `ghub`

--- elpa/ghub-20220918.2022/ghub-pkg.el
+++ elpa/ghub-20220918.2022/ghub-pkg.el
@@ -1,9 +1,9 @@
-(define-package "ghub" "20220810.1157" "Client libraries for Git forge APIs."
+(define-package "ghub" "20220918.2022" "Client libraries for Git forge APIs."
   '((emacs "25.1")
     (compat "28.1.1.0")
     (let-alist "1.0.6")
     (treepy "0.1.1"))
-  :commit "4e5bb75305a763ef9ccfc1971b3cba4520f290d7" :authors
+  :commit "f07790b36beb563d5b87be896efbd3b8533f2acf" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainer
   '("Jonas Bernoulli" . "jonas@bernoul.li")
