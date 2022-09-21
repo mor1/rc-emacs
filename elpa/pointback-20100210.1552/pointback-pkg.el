217283a4f744de044dbc57a0442a48872d7e7d86
Richard Mortier <mort@cantab.net>
2022-09-21 22:47:52 +0100

    emacs: remove `pointback`

--- elpa/pointback-20100210.1552/pointback-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "pointback" "20100210.1552" "Restore window points when returning to buffers" 'nil :stars '(#("1" 0 1 (font-lock-face paradox-star-face))) :commit "e3a02c1784d81b5a1d2477338d049af581ed19f8" :keywords '("convenience") :authors '(("Markus Triska" . "markus.triska@gmx.at")) :maintainer '("Markus Triska" . "markus.triska@gmx.at"))
