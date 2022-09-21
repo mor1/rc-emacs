71306433648d3c271cd802ffc378aa0ccb29a203
Richard Mortier <mort@cantab.net>
2022-09-21 22:47:52 +0100

    emacs: remove `simple-call-tree`

--- elpa/simple-call-tree-20210625.2001/simple-call-tree-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from simple-call-tree.el  -*- no-byte-compile: t -*-
-(define-package "simple-call-tree" "20210625.2001" "analyze source code based on font-lock text-properties" '((emacs "24.3") (anaphora "1.0.0")) :stars '(#("15" 0 2 (font-lock-face paradox-star-face))) :commit "26de24bcde0eae911a0185bb5a6b74b9864fcfc3" :authors '(("Joe Bloggs" . "vapniks@yahoo.com")) :maintainer '("Joe Bloggs" . "vapniks@yahoo.com") :keywords '("programming") :url "http://www.emacswiki.org/emacs/download/simple-call-tree.el")
