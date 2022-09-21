f293c367ff338af2a5f94e46beb09cf57b7ba14a
Richard Mortier <mort@cantab.net>
2022-09-21 22:47:49 +0100

    emacs: remove `clojure-mode`

--- elpa/clojure-mode-20220729.2246/clojure-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from clojure-mode.el  -*- no-byte-compile: t -*-
-(define-package "clojure-mode" "20220729.2246" "Major mode for Clojure code" '((emacs "25.1")) :stars '(#("695" 0 3 (font-lock-face paradox-star-face))) :commit "ad322e989e56c10c05bb286e5b55a82b1e031d62" :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.dev") :keywords '("languages" "clojure" "clojurescript" "lisp") :url "http://github.com/clojure-emacs/clojure-mode")
