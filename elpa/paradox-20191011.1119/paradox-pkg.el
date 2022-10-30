93b077fe4e87bff850c9f0824fa9f466f06f8296
Richard Mortier <mort@cantab.net>
2022-10-30 23:06:01 +0000

    emacs: add `paradox`

--- elpa/paradox-20191011.1119/paradox-pkg.el
+++ elpa/paradox-20191011.1119/paradox-pkg.el
@@ -1,10 +1,10 @@
-(define-package "paradox" "20191011.1111" "A modern Packages Menu. Colored, with package ratings, and customizable."
+(define-package "paradox" "20191011.1119" "A modern Packages Menu. Colored, with package ratings, and customizable."
   '((emacs "24.4")
     (seq "1.7")
     (let-alist "1.0.3")
     (spinner "1.7.3")
     (hydra "0.13.2"))
-  :commit "339fe3518d1d102b2295670340e75caf4f01a29a" :authors
+  :commit "96401577ed02f433debe7604e49afd478e9eda61" :authors
   '(("Artur Malabarba" . "emacs@endlessparentheses.com"))
   :maintainer
   '("Artur Malabarba" . "emacs@endlessparentheses.com")
