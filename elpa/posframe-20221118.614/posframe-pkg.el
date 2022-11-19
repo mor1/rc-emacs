450101b962b82b8299e8baeec3f8e1ae535c367c
Richard Mortier <mort@cantab.net>
2022-11-19 20:45:04 +0000

    emacs: add `posframe`

--- elpa/posframe-20221118.614/posframe-pkg.el
+++ elpa/posframe-20221118.614/posframe-pkg.el
@@ -1,6 +1,6 @@
-(define-package "posframe" "20221015.727" "Pop a posframe (just a frame) at point"
+(define-package "posframe" "20221118.614" "Pop a posframe (just a frame) at point"
   '((emacs "26.1"))
-  :commit "3084cb6eb366d26b0f5a4aa9baffb297178be3b8" :authors
+  :commit "48b7130ca3b5c98bdb21bf99b64a3536acf2b0eb" :authors
   '(("Feng Shu" . "tumashu@163.com"))
   :maintainer
   '("Feng Shu" . "tumashu@163.com")
