be3c98bc997e0fd87b0bac7025d294d3058be627
Richard Mortier <mort@cantab.net>
2022-11-25 20:49:12 +0000

    emacs: add `jsonian`

--- elpa/jsonian-20221119.518/jsonian-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from jsonian.el  -*- no-byte-compile: t -*-
-(define-package "jsonian" "20221119.518" "A major mode for editing JSON files" '((emacs "27.1")) :stars '("") :commit "1e8103c281bf7781bf3246d2049165d8966710c6" :authors '(("Ian Wahbe")) :maintainer '("Ian Wahbe") :url "https://github.com/iwahbe/jsonian")
---
+++ elpa/jsonian-20221121.522/jsonian-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from jsonian.el  -*- no-byte-compile: t -*-
+(define-package "jsonian" "20221121.522" "A major mode for editing JSON files" '((emacs "27.1")) :stars '("") :commit "1d226db4da79d48427b623c2825e499030a6d50f" :authors '(("Ian Wahbe")) :maintainer '("Ian Wahbe") :url "https://github.com/iwahbe/jsonian")
