1a63b2376d564d6a29dafcdb6c88413efa2ddfdd
Richard Mortier <mort@cantab.net>
2022-12-06 22:35:11 +0000

    emacs: add `company`

--- elpa/company-20221206.235/company-pkg.el
+++ elpa/company-20221206.235/company-pkg.el
@@ -1,6 +1,6 @@
-(define-package "company" "20221007.2145" "Modular text completion framework"
+(define-package "company" "20221206.235" "Modular text completion framework"
   '((emacs "25.1"))
-  :commit "48fea7a905b3bcc6d97609316beced666da89b1f" :authors
+  :commit "48f8119db51bed6f50739dbf2cda01b65b354579" :authors
   '(("Nikolaj Schumacher"))
   :maintainer
   '("Dmitry Gutov" . "dgutov@yandex.ru")
