cf4f72e0eb946c557c50f1b8151d0178802d8862
Richard Mortier <mort@cantab.net>
2023-02-28 22:22:05 +0000

    emacs: add `lsp-latex`

---
+++ elpa/lsp-latex-20230121.1846/lsp-latex-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from lsp-latex.el  -*- no-byte-compile: t -*-
+(define-package "lsp-latex" "20230121.1846" "LSP-mode client for LaTeX, on texlab" '((emacs "26.3") (lsp-mode "6.0")) :stars '("") :commit "de080d83f5759ead46dd7a26bb73b7c3a940ef40" :authors '(("ROCKTAKEY" . "rocktakey@gmail.com")) :maintainer '("ROCKTAKEY" . "rocktakey@gmail.com") :keywords '("languages" "tex") :url "https://github.com/ROCKTAKEY/lsp-latex")
