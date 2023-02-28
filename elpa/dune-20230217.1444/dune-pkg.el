4ad6dbe92db2bde9d44449adaa81c0122226fd76
Richard Mortier <mort@cantab.net>
2023-02-28 22:22:03 +0000

    emacs: add `dune`

---
+++ elpa/dune-20230217.1444/dune-pkg.el
@@ -0,0 +1,4 @@
+(define-package "dune" "20230217.1444" "Integration with the dune build system" 'nil :commit "d3d628f2eda2278bd2df6e37452d8693f367fcfd" :url "https://github.com/ocaml/dune")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
