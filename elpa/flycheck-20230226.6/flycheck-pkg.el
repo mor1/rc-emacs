cf671baed39260715e8400b066ee1a96d0ff444d
Richard Mortier <mort@cantab.net>
2023-02-28 22:22:04 +0000

    emacs: add `flycheck`

--- elpa/flycheck-20230226.6/flycheck-pkg.el
+++ elpa/flycheck-20230226.6/flycheck-pkg.el
@@ -1,10 +1,10 @@
-(define-package "flycheck" "20221112.1552" "On-the-fly syntax checking"
-  '((dash "2.12.1")
+(define-package "flycheck" "20230226.6" "On-the-fly syntax checking"
+  '((emacs "25.1")
+    (dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
-    (seq "1.11")
-    (emacs "24.3"))
-  :commit "ab93e2db1dd8333e86f8aeeafbf1e2a0e5f0be38" :authors
+    (seq "1.11"))
+  :commit "2f73050144e1ec0aca312cbe37aee3233ba45217" :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainer
   '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
