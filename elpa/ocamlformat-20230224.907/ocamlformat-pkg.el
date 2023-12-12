34ea499bad26831ab31b301329a0e27ab4089b61
Richard Mortier <mort@cantab.net>
2023-12-12 20:58:55 +0000

    emacs: remove `ocamlformat`

--- elpa/ocamlformat-20230224.907/ocamlformat-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from ocamlformat.el  -*- no-byte-compile: t -*-
-(define-package "ocamlformat" "20230224.907" "Utility functions to format ocaml code" '((emacs "24.3")) :stars '("") :commit "c94ead524c8294a8496b9efde7a6817a9a79f26b" :keywords '("languages" "ocaml") :url "https://github.com/ocaml-ppx/ocamlformat")
