4558997f3ab73a9c776050944f72bb31f4cc89ba
Richard Mortier <mort@cantab.net>
2024-02-23 23:15:23 +0000

    emacs: add `transient`

--- elpa/transient-20240221.1504/transient-pkg.el
+++ elpa/transient-20240221.1504/transient-pkg.el
@@ -1,8 +1,8 @@
-(define-package "transient" "20231216.1908" "Transient commands"
+(define-package "transient" "20240221.1504" "Transient commands"
   '((emacs "26.1")
     (compat "29.1.4.4")
     (seq "2.24"))
-  :commit "72cf67d7e047a4f82e0e18bd016c7826bf2efde0" :authors
+  :commit "ba37702ab3cf90191865172bd5581fd353ccba63" :authors
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
   :maintainers
   '(("Jonas Bernoulli" . "jonas@bernoul.li"))
