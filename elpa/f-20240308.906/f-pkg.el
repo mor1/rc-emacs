151d50d4e3e3ae8666ab4d397445b4b71f80edf0
Richard Mortier <mort@cantab.net>
2024-04-23 12:14:39 +0100

    emacs: add `f`

--- elpa/f-20240308.906/f-pkg.el
+++ elpa/f-20240308.906/f-pkg.el
@@ -1,8 +1,8 @@
-(define-package "f" "20231219.750" "Modern API for working with files and directories"
+(define-package "f" "20240308.906" "Modern API for working with files and directories"
   '((emacs "24.1")
     (s "1.7.0")
     (dash "2.2.0"))
-  :commit "634b0ecdeaa192ef0dd10db312a67ebe5c2d5852" :authors
+  :commit "1e7020dc0d4c52d3da9bd610d431cab13aa02d8c" :authors
   '(("Johan Andersson" . "johan.rejeep@gmail.com"))
   :maintainers
   '(("Lucien Cartier-Tilet" . "lucien@phundrak.com"))
