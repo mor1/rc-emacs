06e8b97bfdd988df94f836239b891816b603fd4f
Richard Mortier <mort@cantab.net>
2024-06-28 20:48:43 +0100

    emacs: add `git-commit`

--- elpa/git-commit-20240623.1335/git-commit-pkg.el
+++ elpa/git-commit-20240623.1335/git-commit-pkg.el
@@ -1,9 +1,9 @@
-(define-package "git-commit" "20240520.1135" "Edit Git commit messages."
+(define-package "git-commit" "20240623.1335" "Edit Git commit messages."
   '((emacs "26.1")
     (compat "29.1.4.5")
     (transient "20240421")
     (with-editor "20240415"))
-  :commit "22fd8f8594202a0679ccc2f7d02bca44d48b8039" :authors
+  :commit "196abe727054a440bf6262c5c254ad4565cbaa66" :authors
   '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
     ("Sebastian Wiesner" . "lunaryorn@gmail.com")
     ("Florian Ragwitz" . "rafl@debian.org")
