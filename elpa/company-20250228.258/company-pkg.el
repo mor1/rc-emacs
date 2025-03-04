c9c12e9b16e45bfeebfd0f5dbad6e98ddad683f6
Richard Mortier <mort@cantab.net>
2025-03-04 10:33:19 +0000

    emacs: add `company`

--- elpa/company-20250228.258/company-pkg.el
+++ elpa/company-20250228.258/company-pkg.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "company" "20240926.2127"
+(define-package "company" "20250228.258"
   "Modular text completion framework."
-  '((emacs "25.1"))
-  :url "https://github.com/company-mode/company-mode"
-  :commit "9c273fc7c1a9dd69ccf508589211c4f8bd0e0765"
-  :revdesc "9c273fc7c1a9"
+  '((emacs "26.1"))
+  :url "http://company-mode.github.io/"
+  :commit "8d599ebc8a9aca27c0a6157aeb31c5b7f05ed0a3"
+  :revdesc "8d599ebc8a9a"
   :keywords '("abbrev" "convenience" "matching")
   :maintainers '(("Dmitry Gutov" . "dmitry@gutov.dev")))
