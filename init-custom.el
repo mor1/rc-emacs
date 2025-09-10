4be5b8ee3c69ba24875d20a5c443b136b6914815
Richard Mortier <mort@cantab.net>
2025-09-10 14:32:53 +0100

    emacs: default font should not set fore/background face

--- init-custom.el
+++ init-custom.el
@@ -207,33 +207,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default
-   ((t
-     (:inherit
-      nil
-      :extend nil
-      :stipple nil
-      :background "#002b36"
-      :foreground "#839496"
-      :inverse-video nil
-      :box nil
-      :strike-through nil
-      :overline nil
-      :underline nil
-      :slant normal
-      :weight regular
-      :height 90
-      :width normal
-      :foundry "PfEd"
-      :family "Atkinson Hyperlegible Mono"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background nil :foreground nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 90 :width normal :foundry "PfEd" :family "Atkinson Hyperlegible Mono"))))
  '(ediff-current-diff-C ((t (:background "yellow1"))))
  '(flyspell-duplicate ((t (:underline (:color "#b58900" :style wave)))))
- '(flyspell-incorrect
-   ((t
-     (:background
-      "#aa0000"
-      :foreground "gray75"
-      :underline (:color "#dc322f" :style wave)))))
+ '(flyspell-incorrect ((t (:background "#aa0000" :foreground "gray75" :underline (:color "#dc322f" :style wave)))))
  '(highlight ((t (:background "dark slate gray"))))
  '(smerge-base ((t (:background "lavender"))))
  '(whitespace-line ((t (:foreground unspecified :underline "OrangeRed3"))))
