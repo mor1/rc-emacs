28669ef009c50ae5931ae146630eaa3f05f46fb9
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:48 +0100

    emacs: add `diff-hl`

---
+++ elpa/diff-hl-20251002.2320/diff-hl-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "diff-hl" "20251002.2320"
+  "Highlight uncommitted changes using VC."
+  '((cl-lib "0.2")
+    (emacs  "26.1"))
+  :url "https://github.com/dgutov/diff-hl"
+  :commit "a44db7a69a17a39d5c51bcf22de4decabf0183ad"
+  :revdesc "a44db7a69a17"
+  :keywords '("vc" "diff")
+  :authors '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainers '(("Dmitry Gutov" . "dmitry@gutov.dev")))
