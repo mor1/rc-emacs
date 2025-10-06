84cb048e56edf43a0a19c85e7e0a2f179a7700cb
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:49 +0100

    emacs: add `nix-ts-mode`

---
+++ elpa/nix-ts-mode-20250819.1504/nix-ts-mode-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "nix-ts-mode" "20250819.1504"
+  "Major mode for Nix expressions, powered by tree-sitter."
+  '((emacs "29.1"))
+  :url "https://github.com/nix-community/nix-ts-mode"
+  :commit "414b18a8a7c9335a1d67c87acc57d5ef2a06a30e"
+  :revdesc "414b18a8a7c9"
+  :keywords '("nix" "languages")
+  :maintainers '(("Remi Gelinas" . "mail@remigelin.as")))
