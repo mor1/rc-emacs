16405567e2a2e9a8f246bde08f299ec0b499ff6f
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:48 +0100

    emacs: add `nano-modeline`

---
+++ elpa/nano-modeline-1.1.0/nano-modeline-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from nano-modeline.el  -*- no-byte-compile: t -*-
+(define-package "nano-modeline" "1.1.0" "N Λ N O modeline" '((emacs "27.1")) :commit "04676d57a1e602123a593836745a744d1b2028fb" :maintainer '("Nicolas P. Rougier" . "Nicolas.Rougier@inria.fr") :keywords '("convenience" "mode-line" "header-line") :url "https://github.com/rougier/nano-modeline")
