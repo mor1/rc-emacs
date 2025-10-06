37de722b421014a202513ed5a90cec9c0e4cc43d
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:49 +0100

    emacs: add `ocp-indent`

---
+++ elpa/ocp-indent-20251001.1320/ocp-indent-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ocp-indent" "20251001.1320"
+  "Automatic indentation with ocp-indent."
+  ()
+  :url "http://www.typerex.org/ocp-indent.html"
+  :commit "8aeb5cc580106366050de0068c11e20f8a947acc"
+  :revdesc "8aeb5cc58010"
+  :keywords '("ocaml" "languages"))
