1fa4b89e12d224ffafda156aa86cbfa2bc060cde
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:47 +0100

    emacs: add `com-css-sort`

---
+++ elpa/com-css-sort-20250101.1004/com-css-sort-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "com-css-sort" "20250101.1004"
+  "Common way of sorting the CSS attributes."
+  '((emacs "25.1")
+    (s     "1.12.0"))
+  :url "https://github.com/jcs-elpa/com-css-sort"
+  :commit "4c6f8bfd88c0bcee9ac121f013d1fcd88f462bae"
+  :revdesc "4c6f8bfd88c0"
+  :keywords '("convenience" "matching" "css" "sort")
+  :authors '(("Jen-Chieh" . "jcs090218@gmail.com"))
+  :maintainers '(("Jen-Chieh" . "jcs090218@gmail.com")))
