3e71ec593fe7d7f083fc862c2354823e2bbbcd93
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:52 +0100

    emacs: add `web-mode`

---
+++ elpa/web-mode-edit-element-20190531.852/web-mode-edit-element-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "web-mode-edit-element" "20190531.852"
+  "Helper-functions for attribute- and element-handling."
+  '((emacs    "24.4")
+    (web-mode "14"))
+  :url "https://github.com/jtkDvlp/web-mode-edit-element"
+  :commit "ad5d7e4dc2420bdd00ce65d9adffbd38a5904afa"
+  :revdesc "ad5d7e4dc242"
+  :keywords '("languages" "convenience")
+  :authors '(("Julian T. Knabenschuh" . "jtkdevelopments@gmail.com"))
+  :maintainers '(("Julian T. Knabenschuh" . "jtkdevelopments@gmail.com")))
