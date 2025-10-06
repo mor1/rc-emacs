973cc3a8943b55a89ac90fafb1417b8e44952d09
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:48 +0100

    emacs: add `corfu`

---
+++ elpa/corfu-20250923.1250/corfu-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "corfu" "20250923.1250"
+  "COmpletion in Region FUnction."
+  '((emacs  "29.1")
+    (compat "30"))
+  :url "https://github.com/minad/corfu"
+  :commit "d6cc682c15b594f2bc9f55196a382a5fda7a7fc6"
+  :revdesc "d6cc682c15b5"
+  :keywords '("abbrev" "convenience" "matching" "completion" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
