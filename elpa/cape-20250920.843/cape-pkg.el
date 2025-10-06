a144555af698da3c24fc65cd8c51526f1ec61fc8
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:47 +0100

    emacs: add `cape`

---
+++ elpa/cape-20250920.843/cape-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "cape" "20250920.843"
+  "Completion At Point Extensions."
+  '((emacs  "29.1")
+    (compat "30"))
+  :url "https://github.com/minad/cape"
+  :commit "e687d4dbf4a8d07a2f1a19b5ca676828038eff31"
+  :revdesc "e687d4dbf4a8"
+  :keywords '("abbrev" "convenience" "matching" "completion" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
