96e4927340ab72175526aa9a57ebf22fd9c78c35
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:51 +0100

    emacs: add `tree-sitter-langs`

---
+++ elpa/tree-sitter-langs-20251005.1125/tree-sitter-langs-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "tree-sitter-langs" "20251005.1125"
+  "Grammar bundle for tree-sitter."
+  '((emacs       "25.1")
+    (tree-sitter "0.15.0"))
+  :url "https://github.com/emacs-tree-sitter/tree-sitter-langs"
+  :commit "222db18b09b3d2576ab0a6c19eaf013008719028"
+  :revdesc "222db18b09b3"
+  :keywords '("languages" "tools" "parsers" "tree-sitter")
+  :authors '(("Tuấn-Anh Nguyễn" . "ubolonton@gmail.com"))
+  :maintainers '(("Jen-Chieh Shen" . "jcs090218@gmail.com")))
