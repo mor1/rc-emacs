1d5c16e9acd42d3a45f16232182f4a0b04737596
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:48 +0100

    emacs: add `jinx`

---
+++ elpa/jinx-20250920.956/jinx-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "jinx" "20250920.956"
+  "Enchanted Spell Checker."
+  '((emacs  "29.1")
+    (compat "30"))
+  :url "https://github.com/minad/jinx"
+  :commit "55c844066601ec1a4741ff8f459ff6321f6935b7"
+  :revdesc "55c844066601"
+  :keywords '("convenience" "text")
+  :authors '(("Daniel Mendler" . "mail@daniel-mendler.de"))
+  :maintainers '(("Daniel Mendler" . "mail@daniel-mendler.de")))
