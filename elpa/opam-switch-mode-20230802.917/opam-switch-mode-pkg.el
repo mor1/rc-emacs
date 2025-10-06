d1eecb3c0fe320af290ea3c674591bc259548c73
Richard Mortier <mort@cantab.net>
2025-10-06 16:53:49 +0100

    emacs: add `opam-switch-mode`

---
+++ elpa/opam-switch-mode-20230802.917/opam-switch-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "opam-switch-mode" "20230802.917"
+  "Select OCaml opam switches via a menu."
+  '((emacs "25.1"))
+  :url "https://github.com/ProofGeneral/opam-switch-mode"
+  :commit "1069e56a662f23ea09d4e05611bdedeb99257012"
+  :revdesc "1069e56a662f"
+  :maintainers '((nil . "proof-general-maintainers@groupes.renater.fr")))
