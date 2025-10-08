86f8295052f6cf5b56245bbe24607fa1dd8d7312
Richard Mortier <mort@cantab.net>
2025-10-08 23:35:53 +0100

    emacs: remove `filladapt`

--- elpa/filladapt-2.12.2/filladapt-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from filladapt.el  -*- no-byte-compile: t -*-
-(define-package "filladapt" "2.12.2" "Adaptive fill" '((emacs "24.4")) :url "http://elpa.gnu.org/packages/filladapt.html" :authors '(("Kyle E. Jones" . "kyle_jones@wonderworks.com")) :maintainer '(nil . "emacs-devel@gnu.org"))
