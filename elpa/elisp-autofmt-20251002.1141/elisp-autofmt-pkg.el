99c9f6a00a8e6cc01c5de067e60881bc16cc6f9c
Richard Mortier <mort@cantab.net>
2025-10-12 12:36:07 +0100

    emacs: add `elisp-autofmt`

--- elpa/elisp-autofmt-20251002.1141/elisp-autofmt-pkg.el
+++ elpa/elisp-autofmt-20251002.1141/elisp-autofmt-pkg.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "elisp-autofmt" "20250611.2328"
+(define-package "elisp-autofmt" "20251002.1141"
   "Emacs lisp auto-format."
   '((emacs "29.1"))
   :url "https://codeberg.org/ideasman42/emacs-elisp-autofmt"
-  :commit "c4cd57b2599ec82a4363ae322aa734e34a719404"
-  :revdesc "c4cd57b2599e"
+  :commit "6f5936074bb3e44437957cef1f5405898a60309c"
+  :revdesc "6f5936074bb3"
   :authors '(("Campbell Barton" . "ideasman42@gmail.com"))
   :maintainers '(("Campbell Barton" . "ideasman42@gmail.com")))
