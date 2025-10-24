d4de96a77985cc6bb058d08d0a7ba8a0a9dc51ce
Richard Mortier <mort@cantab.net>
2025-10-24 15:56:06 +0100

    emacs: add `markdown-ts-mode`

---
+++ elpa/markdown-ts-mode-20240422.2329/markdown-ts-mode-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "markdown-ts-mode" "20240422.2329"
+  "Major mode for Markdown using Treesitter."
+  '((emacs "29.1"))
+  :url "https://github.com/LionyxML/markdown-ts-mode"
+  :commit "2f1ee8b94cdf53cebc31ae08ecfbba846193d5e1"
+  :revdesc "2f1ee8b94cdf"
+  :keywords '("languages" "matching" "faces"))
