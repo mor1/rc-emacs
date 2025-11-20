3fbdd64717f3965cad2b8cd0b6bd0a6b021a31cb
Richard Mortier <mort@cantab.net>
2025-11-20 16:21:34 +0000

    emacs: add `marginalia`

--- elpa/marginalia-20251115.1821/marginalia-pkg.el
+++ elpa/marginalia-20251115.1821/marginalia-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "marginalia" "20251013.932"
+(define-package "marginalia" "20251115.1821"
   "Enrich existing commands with completion annotations."
   '((emacs  "29.1")
     (compat "30"))
   :url "https://github.com/minad/marginalia"
-  :commit "8a07e869577e0b8582db2528b08ec295d0405bb7"
-  :revdesc "8a07e869577e"
+  :commit "f4058af89839566495d6037d35489da6eee45250"
+  :revdesc "f4058af89839"
   :keywords '("docs" "help" "matching" "completion")
   :authors '(("Omar Antolín Camarena" . "omar@matem.unam.mx")
              ("Daniel Mendler" . "mail@daniel-mendler.de"))
