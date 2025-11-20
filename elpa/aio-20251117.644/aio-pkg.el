5f16578f0ddcff4929ad5325b5b02a006dbcbd20
Richard Mortier <mort@cantab.net>
2025-11-20 16:21:33 +0000

    emacs: add `aio`

--- elpa/aio-20251117.644/aio-pkg.el
+++ elpa/aio-20251117.644/aio-pkg.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "aio" "20200610.1904"
+(define-package "aio" "20251117.644"
   "Async/await for Emacs Lisp."
   '((emacs "26.1"))
   :url "https://github.com/skeeto/emacs-aio"
-  :commit "da93523e235529fa97d6f251319d9e1d6fc24a41"
-  :revdesc "da93523e2355"
+  :commit "58157e51e7eb7a4b954894ee4182564c507a2f01"
+  :revdesc "58157e51e7eb"
   :authors '(("Christopher Wellons" . "wellons@nullprogram.com"))
   :maintainers '(("Christopher Wellons" . "wellons@nullprogram.com")))
