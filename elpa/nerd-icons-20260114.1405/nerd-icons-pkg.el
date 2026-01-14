634e036dd05c877e095c6ab54b483bd0a69ebf1b
Richard Mortier <mort@cantab.net>
2026-01-14 15:00:44 +0000

    emacs: add `nerd-icons`

--- elpa/nerd-icons-20260114.1405/nerd-icons-pkg.el
+++ elpa/nerd-icons-20260114.1405/nerd-icons-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "nerd-icons" "20251108.1545"
+(define-package "nerd-icons" "20260114.1405"
   "Emacs Nerd Font Icons Library."
   '((emacs "25.1"))
   :url "https://github.com/rainstormstudio/nerd-icons.el"
-  :commit "940b28d3dbd134696ef448e906e42a87a9d61e20"
-  :revdesc "940b28d3dbd1"
+  :commit "a57f80901590a13871e4c9a0238d8b5be08e15e1"
+  :revdesc "a57f80901590"
   :keywords '("lisp")
   :authors '(("Hongyu Ding" . "rainstormstudio@yahoo.com")
              ("Vincent Zhang" . "seagle0128@gmail.com"))
