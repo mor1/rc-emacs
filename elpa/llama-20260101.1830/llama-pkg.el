6bb5c6a43ef4f004101c0f4eb4640f7e83bf6c2f
Richard Mortier <mort@cantab.net>
2026-01-14 15:00:44 +0000

    emacs: add `llama`

--- elpa/llama-20260101.1830/llama-pkg.el
+++ elpa/llama-20260101.1830/llama-pkg.el
@@ -1,9 +1,9 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "llama" "20251101.2002"
+(define-package "llama" "20260101.1830"
   "Compact syntax for short lambda."
   '((emacs  "26.1")
     (compat "30.1"))
   :url "https://github.com/tarsius/llama"
-  :commit "e4803de8ab85991b6a944430bb4f543ea338636d"
-  :revdesc "e4803de8ab85"
+  :commit "2a89ba755b0459914a44b1ffa793e57f759a5b85"
+  :revdesc "2a89ba755b04"
   :keywords '("extensions"))
