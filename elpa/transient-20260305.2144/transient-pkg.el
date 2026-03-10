9e02e1e0beb1f818a43d61126c96ccb5ababf9c3
Richard Mortier <mort@cantab.net>
2026-03-10 12:24:53 +0000

    emacs: add `transient`

--- elpa/transient-20260305.2144/transient-pkg.el
+++ elpa/transient-20260305.2144/transient-pkg.el
@@ -1,13 +1,13 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "transient" "20260226.1256"
+(define-package "transient" "20260305.2144"
   "Transient commands."
   '((emacs    "28.1")
     (compat   "30.1")
     (cond-let "0.2")
     (seq      "2.24"))
   :url "https://github.com/magit/transient"
-  :commit "ef7714b2528b7c74034a1568a753a56dcc8ca877"
-  :revdesc "ef7714b2528b"
+  :commit "e7cae38a1fd613c5dae43a60de14a72a9b48bc9c"
+  :revdesc "e7cae38a1fd6"
   :keywords '("extensions")
   :authors '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev"))
   :maintainers '(("Jonas Bernoulli" . "emacs.transient@jonas.bernoulli.dev")))
