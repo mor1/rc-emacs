86cb900f45bc80631689c263d2bbc9374b96c75b
Richard Mortier <mort@cantab.net>
2026-03-10 12:24:52 +0000

    emacs: add `markdown-mode`

--- elpa/markdown-mode-20260308.930/markdown-mode-pkg.el
+++ elpa/markdown-mode-20260308.930/markdown-mode-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "markdown-mode" "20260226.609"
+(define-package "markdown-mode" "20260308.930"
   "Major mode for Markdown-formatted text."
   '((emacs "28.1"))
   :url "https://jblevins.org/projects/markdown-mode/"
-  :commit "3933f282d0dbce79355d47c97226ab641028d1c0"
-  :revdesc "3933f282d0db"
+  :commit "107a368a6deffa943544c220b5a6b1304ffc9945"
+  :revdesc "107a368a6def"
   :keywords '("markdown" "github flavored markdown" "itex")
   :authors '(("Jason R. Blevins" . "jblevins@xbeta.org"))
   :maintainers '(("Jason R. Blevins" . "jblevins@xbeta.org")))
