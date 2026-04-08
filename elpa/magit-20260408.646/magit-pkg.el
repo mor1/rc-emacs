0b8b3716d084518caec16a2c519e2ad88686ad8f
Richard Mortier <mort@cantab.net>
2026-04-08 23:56:21 +0100

    emacs: add `magit`

---
+++ elpa/magit-20260408.646/magit-pkg.el
@@ -0,0 +1,19 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "magit" "20260408.646"
+  "A Git porcelain inside Emacs."
+  '((emacs         "28.1")
+    (compat        "30.1")
+    (cond-let      "0.2")
+    (llama         "1.0")
+    (magit-section "4.5")
+    (seq           "2.24")
+    (transient     "0.12")
+    (with-editor   "3.4"))
+  :url "https://github.com/magit/magit"
+  :commit "073b01e83405031da1ed55b45cb6b649a3cfb228"
+  :revdesc "073b01e83405"
+  :keywords '("git" "tools" "vc")
+  :authors '(("Marius Vollmer" . "marius.vollmer@gmail.com")
+             ("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+                 ("Kyle Meyer" . "kyle@kyleam.com")))
